a4b50906d6343b35709337e7c6e7602a9fe8878f
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-04-03 10:57:46 +0200

    Tarjeta de IntelliJ y vídeo mejoradas

--- resources/views/tarjetas/intellij_project.blade.php
+++ resources/views/tarjetas/intellij_project.blade.php
@@ -1,16 +1,18 @@
 <div class="card">
-    <div class="card-header">Proyecto de IntelliJ</div>
+    <div class="card-header"><i class="fab fa-java"></i> {{ __('IntelliJ project') }}</div>
     <div class="card-body">
         <h5 class="card-title">{{ $repositorio['name'] }}</h5>
         <p class="card-text">{{ $repositorio['description'] }}</p>
         @if(!$intellij_project->isForked())
-            <a href="{{ route('intellij_projects.fork', ['actividad' => $actividad->id, 'intellij_project'=>$intellij_project->id]) }}"
-               class="btn btn-primary">Clonar el repositorio</a>
+            {{--
+                        <a href="{{ route('intellij_projects.fork', ['actividad' => $actividad->id, 'intellij_project'=>$intellij_project->id]) }}"
+                           class="btn btn-primary">{{ __('Clone the project') }}</a>
+            --}}
         @else
             <a href="jetbrains://idea/checkout/git?checkout.repo={{ str_replace('https://',"https://$user->username@",$repositorio['http_url_to_repo']) }}&idea.required.plugins.id=Git4Idea"
-               class="btn btn-primary">Abrir en IntelliJ IDEA</a>
+               class="btn btn-primary">{{ __('Open in IntelliJ IDEA') }}</a>
             <a href="{{ $repositorio['web_url']  }}" target="_blank"
-               class="btn btn-link text-secondary">Ver en GitLab</a>
+               class="btn btn-link text-secondary">{{ __('Open in GitLab') }}</a>
         @endif
     </div>
 </div>
--- resources/views/tarjetas/youtube_video.blade.php
+++ resources/views/tarjetas/youtube_video.blade.php
@@ -1,9 +1,9 @@
 <div class="card">
-    <div class="card-header">Vídeo</div>
+    <div class="card-header"><i class="fas fa-video"></i> {{ __('Video') }}</div>
     <div class="card-body">
         <h5 class="card-title">{{ $youtube_video->titulo }}</h5>
         <p class="card-text">{{ $youtube_video->descripcion }}</p>
-        <div class="p-1" style="background-color:#eee;">
+        <div class="p-1 mb-1" style="background-color:#eee;">
             <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item"
                         src="https://www.youtube.com/embed/{{ $youtube_video->codigo }}?rel=0&modestbranding=1"
@@ -11,10 +11,12 @@
                         allowfullscreen></iframe>
             </div>
         </div>
-        <form>
-            <div class="card-text mt-3">
-                <button type="submit" class="btn btn-primary">Marcar como visto</button>
-            </div>
-        </form>
+        {{--
+                <form>
+                    <div class="card-text mt-3">
+                        <button type="submit" class="btn btn-primary">Marcar como visto</button>
+                    </div>
+                </form>
+        --}}
     </div>
 </div>
