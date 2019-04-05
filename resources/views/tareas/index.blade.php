e24690f833036d9a1b9cb973d981c6a023750bd1
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-04-05 14:06:04 +0200

    Selector de unidad mejorado

--- resources/views/tareas/index.blade.php
+++ resources/views/tareas/index.blade.php
@@ -111,16 +111,16 @@
         <div class="col-md-12">
             {!! Form::open(['route' => ['tareas.index', $user->id]]) !!}
 
-            <div class="form-group row">
-                {!! Form::label('unidad', __('Unit'), ['class' => 'col-sm-2 col-form-label']) !!}
-                <div class="col-sm-8">
+            <div class="form-group d-flex flex-row justify-content-between">
+                {!! Form::label('unidad', __('Unit'), ['class' => 'col-form-label']) !!}
+                <div class="flex-fill mx-3">
                     <select class="form-control" id="unidad_id" name="unidad_id">
                         @foreach($unidades as $unidad)
                             <option value="{{ $unidad->id }}" {{ session('profesor_unidad_actual') == $unidad->id ? 'selected' : '' }}>{{ $unidad->nombre }}</option>
                         @endforeach
                     </select>
                 </div>
-                <div class="col-sm-2 mt-3 mt-sm-0">
+                <div class="mt-3 mt-sm-0">
                     <button type="submit" class="btn btn-primary">{{ __('Change') }}</button>
                 </div>
             </div>
