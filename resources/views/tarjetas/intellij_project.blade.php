f7013ed8830e749d0cf22d96009604c6ae73c80c
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-04-07 23:40:02 +0200

    Usuario actual al abrir un repositorio

--- resources/views/tarjetas/intellij_project.blade.php
+++ resources/views/tarjetas/intellij_project.blade.php
@@ -7,7 +7,7 @@
             <a href="{{ route('intellij_projects.fork', ['actividad' => $actividad->id, 'intellij_project'=>$intellij_project->id]) }}"
                class="btn btn-primary">{{ __('Clone the project') }}</a>
         @else
-            <a href="jetbrains://idea/checkout/git?checkout.repo={{ str_replace('https://',"https://$user->username@",$repositorio['http_url_to_repo']) }}&idea.required.plugins.id=Git4Idea"
+            <a href="jetbrains://idea/checkout/git?checkout.repo={{ str_replace('https://',"https://".Auth::user()->username."@",$repositorio['http_url_to_repo']) }}&idea.required.plugins.id=Git4Idea"
                class="btn btn-primary">{{ __('Open in IntelliJ IDEA') }}</a>
             <a href="{{ $repositorio['web_url']  }}" target="_blank"
                class="btn btn-link text-secondary">{{ __('Open in GitLab') }}</a>
