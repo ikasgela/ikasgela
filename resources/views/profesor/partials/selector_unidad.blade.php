b4f3127635fb2075ddbc2b3a4fead074b431f1a9
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-04-08 01:08:17 +0200

    Filtro de unidad mejorado

--- resources/views/profesor/partials/selector_unidad.blade.php
+++ resources/views/profesor/partials/selector_unidad.blade.php
@@ -6,13 +6,14 @@
             {!! Form::label('unidad', __('Unit'), ['class' => 'col-form-label']) !!}
             <div class="flex-fill mx-3">
                 <select class="form-control" id="unidad_id" name="unidad_id">
+                    <option value="">{{ __('--- None ---') }}</option>
                     @foreach($unidades as $unidad)
                         <option value="{{ $unidad->id }}" {{ session('profesor_unidad_actual') == $unidad->id ? 'selected' : '' }}>{{ $unidad->nombre }}</option>
                     @endforeach
                 </select>
             </div>
-            <div class="mt-3 mt-sm-0">
-                <button type="submit" class="btn btn-primary">{{ __('Change') }}</button>
+            <div>
+                <button type="submit" class="btn btn-primary">{{ __('Filter') }}</button>
             </div>
         </div>
 
