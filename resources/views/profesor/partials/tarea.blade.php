80dc98d2a7d7c1d04f311b749e41b0c52c464be9
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-04-08 13:30:24 +0200

    Página de correción mejorada

--- resources/views/profesor/partials/tarea.blade.php
+++ resources/views/profesor/partials/tarea.blade.php
@@ -6,59 +6,53 @@
 @endif
 @include('profesor.partials.tarjeta_usuario')
 <div class="row mt-4">
-    <form class="col-md-12 p-0 form-inline"
-          method="POST"
-          action="{{ route('actividades.estado', [$tarea->id]) }}">
-        @csrf
-        @method('PUT')
+    <div class="col-md-12">
+        {{-- Tarjeta --}}
+        <div class="card border-dark">
+            <div class="card-header text-white bg-dark d-flex justify-content-between">
+                <span>{{ $actividad->unidad->curso->nombre }} » {{ $actividad->unidad->nombre }}</span>
+            </div>
+            <form class="col-md-12 p-0"
+                  method="POST"
+                  action="{{ route('actividades.estado', [$tarea->id]) }}">
+                @csrf
+                @method('PUT')
 
-        <div class="col-md-12">
-            {{-- Tarjeta --}}
-            <div class="card border-dark">
-                <div class="card-header text-white bg-dark d-flex justify-content-between">
-                    <span>{{ $actividad->unidad->curso->nombre }} » {{ $actividad->unidad->nombre }}</span>
-                </div>
                 <div class="card-body">
                     <h2>{{ $actividad->nombre }}</h2>
                     <p>{{ $actividad->descripcion }}</p>
                     <div>
                         <button type="submit" name="nuevoestado" value="40"
-                                class="btn btn-success"> {{ __('Ok') }}
+                                class="btn btn-primary"> {{ __('Finished') }}
                         </button>
                         <button type="submit" name="nuevoestado" value="41"
-                                class="btn btn-danger"> {{ __('Error') }}
+                                class="btn btn-warning"> {{ __('Send again') }}
                         </button>
                     </div>
+                    <i class="fas fa-bullhorn mt-3"></i>
+                    <label class="m-0" for="feedback">{{ __('Feedback') }}</label>
+                    <textarea class="form-control"
+                              id="feedback"
+                              name="feedback"
+                              rows="10">{{ $tarea->feedback }}</textarea>
                 </div>
-            </div>
+            </form>
             {{-- Fin tarjeta--}}
         </div>
-        @if($tarea->estado > 10)
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <i class="fas fa-bullhorn"></i>
-                        <label class="m-0" for="feedback">{{ __('Feedback') }}</label>
-                    </div>
-                    <div class="card-body">
-                                <textarea class="form-control"
-                                          id="feedback"
-                                          name="feedback"
-                                          rows="3">{{ $tarea->feedback }}</textarea>
-                    </div>
-                </div>
+    </div>
+    @if($tarea->estado > 10)
+        <div class="col-md-12">
+        </div>
+        @foreach($actividad->youtube_videos()->get() as $youtube_video)
+            <div class="col-md-6">
+                @include('tarjetas.youtube_video')
             </div>
-            @foreach($actividad->youtube_videos()->get() as $youtube_video)
-                <div class="col-md-6">
-                    @include('tarjetas.youtube_video')
-                </div>
-            @endforeach
-            @foreach($actividad->intellij_projects()->get() as $intellij_project)
-                <div class="col-md-6">
-                    @php($repositorio = $intellij_project->gitlab())
-                    @include('tarjetas.intellij_project')
-                </div>
-            @endforeach
-        @endif
-    </form>
+        @endforeach
+        @foreach($actividad->intellij_projects()->get() as $intellij_project)
+            <div class="col-md-6">
+                @php($repositorio = $intellij_project->gitlab())
+                @include('tarjetas.intellij_project')
+            </div>
+        @endforeach
+    @endif
 </div>
