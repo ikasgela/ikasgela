6dccb87261b3b58b81ac236b346a47ddb9c92dd0
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-04-10 20:49:40 +0200

    Nuevo logo en negro

--- resources/views/partials/logos.blade.php
+++ resources/views/partials/logos.blade.php
@@ -1,2 +1,6 @@
-<img class="navbar-brand-full" src="{{ url('/svg/logo.svg') }}" height="25" alt="{{ __('Logo') }}">
-<img class="navbar-brand-minimized" src="{{ url('/svg/icono.svg') }}" width="30" alt="{{ __('Logo') }}">
+<img class="navbar-brand-full"
+     src="{{ config('app.debug') ? '/svg/logo-black.svg' : '/svg/logo.svg' }}" height="25"
+     alt="{{ __('Logo') }}">
+<img class="navbar-brand-minimized"
+     src="{{ url('/svg/icono.svg') }}" width="30"
+     alt="{{ __('Logo') }}">
