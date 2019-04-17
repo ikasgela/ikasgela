36d4c5d95f2d13eef8c64e5aad36f18dc5e146da
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-04-18 00:15:33 +0200

    Botones de recursos más contrastados

--- resources/views/partials/botones_recursos.blade.php
+++ resources/views/partials/botones_recursos.blade.php
@@ -2,12 +2,12 @@
     <div class='btn-group'>
         <a title="{{ __('YouTube video') }}"
            href="{{ route('youtube_videos.actividad', [$actividad->id]) }}"
-           class="btn btn-light btn-sm {{ $actividad->youtube_videos()->count() > 0 ? 'text-primary' : '' }}">
+           class="btn btn-sm {{ $actividad->youtube_videos()->count() > 0 ? 'btn-primary' : 'btn-light' }}">
             <i class="fab fa-youtube"></i>
         </a>
         <a title="{{ __('IntelliJ project') }}"
            href="{{ route('intellij_projects.actividad', [$actividad->id]) }}"
-           class="btn btn-light btn-sm {{ $actividad->intellij_projects()->count() > 0 ? 'text-primary' : '' }}">
+           class="btn btn-sm {{ $actividad->intellij_projects()->count() > 0 ? 'btn-primary' : 'btn-light' }}">
             <i class="fab fa-java"></i></a>
     </div>
 </td>
