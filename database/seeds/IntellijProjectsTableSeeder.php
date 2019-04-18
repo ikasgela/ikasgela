2e413382c04220397789508b1e36913472894075
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-04-18 18:05:30 +0200

    Modificado el seed de proyectos

--- database/seeds/IntellijProjectsTableSeeder.php
+++ database/seeds/IntellijProjectsTableSeeder.php
@@ -13,18 +13,23 @@ class IntellijProjectsTableSeeder extends Seeder
     public function run()
     {
         $proyecto = new IntellijProject();
+        $proyecto->titulo = '¡Hola mundo!';
+        $proyecto->descripcion = 'Primer proyecto de prueba.';
         $proyecto->repositorio = 'programacion/introduccion/hola-mundo';
         $proyecto->save();
 
         $proyecto = new IntellijProject();
+        $proyecto->titulo = 'Agenda';
         $proyecto->repositorio = 'programacion/gui/agenda';
         $proyecto->save();
 
         $proyecto = new IntellijProject();
+        $proyecto->titulo = 'Tres en raya';
         $proyecto->repositorio = 'programacion/gui/tres-en-raya';
         $proyecto->save();
 
         $proyecto = new IntellijProject();
+        $proyecto->titulo = 'Reservas';
         $proyecto->repositorio = 'programacion/colecciones/reservas';
         $proyecto->save();
     }
