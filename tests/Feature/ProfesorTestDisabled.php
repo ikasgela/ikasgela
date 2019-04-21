18589a67e98b1569dbea911c3633c33aba148be8
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-04-21 02:38:25 +0200

    Desactivados los tests del profesor

--- tests/Feature/ProfesorTestDisabled.php
+++ tests/Feature/ProfesorTestDisabled.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
-class ProfesorTest extends TestCase
+class ProfesorTestDisabled extends TestCase
 {
     use DatabaseMigrations;
 
