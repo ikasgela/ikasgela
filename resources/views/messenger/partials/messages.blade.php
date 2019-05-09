f6cbc5e634665bf79d3a84fb8f191c028aae43e1
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-05-09 13:29:15 +0200

    Mostrar los saltos de línea en los mensajes

--- resources/views/messenger/partials/messages.blade.php
+++ resources/views/messenger/partials/messages.blade.php
@@ -1,9 +1,9 @@
 <div class="media border rounded p-3 mb-3 bg-white">
-    <img src="//www.gravatar.com/avatar/{{ md5($message->user->email) }}?s=64"
+    <img width="64" src="//www.gravatar.com/avatar/{{ md5($message->user->email) }}?s=128"
          alt="{{ $message->user->name }}" class="img-circle">
     <div class="media-body pl-3">
         <h5 class="media-heading">{{ $message->user->name }}</h5>
-        <p>{{ $message->body }}</p>
+        <pre>{{ $message->body }}</pre>
         <div class="text-muted">
             <small>{{ __('Posted') }} {{ $message->created_at->diffForHumans() }}</small>
         </div>
--- resources/views/messenger/partials/thread.blade.php
+++ resources/views/messenger/partials/thread.blade.php
@@ -8,6 +8,6 @@
     <div class="card-body pb-1">
         <h5 class="card-title"><a href="{{ route('messages.show', $thread->id) }}">{{ $thread->subject }}</a></h5>
         <small class="text-muted">{{ __('Latest message') }}</small>
-        <p class="bg-light p-2 rounded">{{ $thread->latestMessage->body }}</p>
+        <pre class="bg-light p-3 rounded">{{ $thread->latestMessage->body }}</pre>
     </div>
 </div>
