d52bb45f9c27380ba9ff22a68c637dc679d22578
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-05-19 16:57:57 +0200

    Seeders reordenados

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,19 +15,18 @@ class DatabaseSeeder extends Seeder
         $this->call(PeriodsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(CursosTableSeeder::class);
-
         $this->call(UnidadesTableSeeder::class);
-        $this->call(ActividadesTableSeeder::class);
-
-        $this->call(YoutubeVideosTableSeeder::class);
-        $this->call(IntellijProjectsTableSeeder::class);
 
         $this->call(GroupsTableSeeder::class);
         $this->call(TeamsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
 
+        $this->call(YoutubeVideosTableSeeder::class);
+        $this->call(IntellijProjectsTableSeeder::class);
+
+        $this->call(ActividadesTableSeeder::class);
         $this->call(TareasTableSeeder::class);
-        $this->call(RegistrosTableSeeder::class);
+        //$this->call(RegistrosTableSeeder::class);
     }
 }
