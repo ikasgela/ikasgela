f05a6c230065392d8fcc24cec7e0486b8979f996
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-05-21 11:21:09 +0200

    Seed de los RAs de Programación

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -30,5 +30,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RegistrosTableSeeder::class);
 
         $this->call(QualificationsTableSeeder::class);
+        $this->call(SkillsTableSeeder::class);
     }
 }
---
+++ database/seeds/SkillsTableSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SkillsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('skills')->insert(array(
+            3 =>
+                array(
+                    'id' => 4,
+                    'name' => 'RA1 - Identificación de los elementos de un programa informático',
+                    'description' => 'Reconoce la estructura de un programa informático, identificando y relacionando los elementos propios del lenguaje de programación utilizado.',
+                ),
+            4 =>
+                array(
+                    'id' => 5,
+                    'name' => 'RA2 - Escritura y prueba de programas sencillos',
+                    'description' => 'Escribe y prueba programas sencillos, reconociendo y aplicando los fundamentos de la programación orientada a objetos.',
+                ),
+            5 =>
+                array(
+                    'id' => 6,
+                    'name' => 'RA3 - Escritura de programas utilizando las estructuras de control del lenguaje',
+                    'description' => 'Escribe y depura código, analizando y utilizando las estructuras de control del lenguaje.',
+                ),
+            6 =>
+                array(
+                    'id' => 7,
+                    'name' => 'RA4 - Realización de programas aplicando los principios de la programación orientada a objetos',
+                    'description' => 'Desarrolla programas organizados en clases, analizando y aplicando los principios de la programación orientada a objetos.',
+                ),
+            7 =>
+                array(
+                    'id' => 8,
+                    'name' => 'RA5 - Realización de programas utilizando interfaces gráficos',
+                    'description' => 'Realiza  operaciones de entrada  y salida  de información,  utilizando procedimientos específicos del lenguaje y librerías de clases.',
+                ),
+            8 =>
+                array(
+                    'id' => 9,
+                    'name' => 'RA6 - Desarrollo de programas que utilicen tipos avanzados de datos',
+                    'description' => 'Escribe  programas  que  manipulen  información,  seleccionando  y  utilizando  tipos avanzados de datos.',
+                ),
+            9 =>
+                array(
+                    'id' => 10,
+                    'name' => 'RA7 - Realización de programas que utilicen características avanzadas de los lenguajes orientados a objetos',
+                    'description' => 'Desarrolla programas aplicando características avanzadas de los lenguajes orientados a objetos y del entorno de programación.',
+                ),
+            10 =>
+                array(
+                    'id' => 11,
+                    'name' => 'RA8 - Utilización de bases de datos orientadas a objetos',
+                    'description' => 'Utiliza  Bases  de  Datos  Orientadas  a  Objetos,  analizando  sus  características  y aplicando técnicas para mantener la persistencia de la información.',
+                ),
+            11 =>
+                array(
+                    'id' => 12,
+                    'name' => 'RA9 - Utilización de bases de datos relacionales',
+                    'description' => 'Gestiona  información  almacenada  en  bases  de  datos  relacionales  manteniendo  la integridad y consistencia de los datos.',
+                ),
+        ));
+
+    }
+}
