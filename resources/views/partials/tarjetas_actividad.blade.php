143f511395d9c533c2050858edd250cc7a2e12c5
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-05-27 14:27:12 +0200

    Actividades reordenadas

--- resources/views/partials/tarjetas_actividad.blade.php
+++ resources/views/partials/tarjetas_actividad.blade.php
@@ -3,14 +3,14 @@
         @include('markdown_texts.tarjeta', ['texto' => $markdown_text->markdown()])
     </div>
 @endforeach
-@foreach($actividad->cuestionarios()->get() as $cuestionario)
-    <div class="col-md-6">
-        @include('cuestionarios.tarjeta')
-    </div>
-@endforeach
 @foreach($actividad->youtube_videos()->get() as $youtube_video)
     <div class="col-md-6">
         @include('tarjetas.youtube_video')
+    </div>
+@endforeach
+@foreach($actividad->cuestionarios()->get() as $cuestionario)
+    <div class="col-md-6">
+        @include('cuestionarios.tarjeta')
     </div>
 @endforeach
 @foreach($actividad->intellij_projects()->get() as $intellij_project)
