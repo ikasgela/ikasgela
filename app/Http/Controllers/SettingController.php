5d92efe1fcd9305695d4a50dcaaab545ed7cb503
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-06-11 13:02:44 +0200

    Selección de curso

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Organization;
+use App\Period;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,11 +12,17 @@ class SettingController extends Controller
     public function editar()
     {
         $user = Auth::user();
-        $cursos = $user->cursos;
+
+        $organizations = $user->organizations()->orderBy('name')->get();
+
+        $filtro = $user->organizations()->pluck('organization_id')->unique()->flatten()->toArray();
+        $periods = Period::whereIn('id', $filtro)->orderBy('name')->get();
+
+        $cursos = $user->cursos()->orderBy('nombre')->get();
 
         setting()->setExtraColumns(['user_id' => $user->id]);
 
-        return view('settings.edit', compact(['cursos', 'user']));
+        return view('settings.edit', compact(['user', 'cursos', 'organizations', 'periods']));
     }
 
     public function guardar(Request $request)
@@ -24,7 +32,11 @@ class SettingController extends Controller
         ]);
 
         setting()->setExtraColumns(['user_id' => Auth::user()->id]);
+
+        setting(['organization_actual' => $request->input('organization_id')]);
+        setting(['period_actual' => $request->input('period_id')]);
         setting(['curso_actual' => $request->input('curso_id')]);
+
         setting()->save();
 
         return redirect(route('settings.editar'));
