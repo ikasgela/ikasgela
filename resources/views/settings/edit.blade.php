e01288a6398bb2bacbb4977c0288e372ba5faa1d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-06-12 15:00:35 +0200

    No mostrar el periodo en los ajustes

--- resources/views/settings/edit.blade.php
+++ resources/views/settings/edit.blade.php
@@ -20,17 +20,6 @@
                             </select>
                         </div>
                     </div>
-
-                    <div class="form-group row">
-                        {!! Form::label('period_id', __('Period'), ['class' => 'col-sm-2 col-form-label']) !!}
-                        <div class="col-sm-10">
-                            <select class="form-control" id="period_id" name="period_id">
-                                @foreach($periods as $period)
-                                    <option value="{{ $period->id }}" <?php if (@setting('period_actual') == $period->id) echo 'selected'; ?>>{{ $period->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
                 @endif
             @endauth
 
