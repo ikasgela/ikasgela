fa494209c3f02c2951fddac92ee7b3ca4f3f1645
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-06-12 19:07:51 +0200

    Prueba de Vimeo

--- resources/views/tarjetas/video.blade.php
+++ resources/views/tarjetas/video.blade.php
@@ -13,7 +13,7 @@
 
             {{-- Tarjeta --}}
             <div class="card">
-                <div class="card-header">Vídeo</div>
+                <div class="card-header">YouTube</div>
                 <div class="card-body">
                     <h5 class="card-title">El bucle while</h5>
                     <h6 class="card-subtitle text-muted mb-2">Estructuras de control</h6>
@@ -37,7 +37,7 @@
 
             {{-- Tarjeta --}}
             <div class="card">
-                <div class="card-header">Vídeo</div>
+                <div class="card-header">Vimeo</div>
                 <div class="card-body">
                     <h5 class="card-title">Listas - Ejercicio resuelto: Ticket</h5>
                     <h6 class="card-subtitle text-muted mb-2">Estructuras de datos II</h6>
@@ -45,10 +45,8 @@
                         compuesto de líneas de ticket, almacenadas en un
                         <code>List<></code>.</p>
                     <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item"
-                                src="https://www.youtube.com/embed/bgkk4QbW4QM?rel=0&modestbranding=1"
-                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen></iframe>
+                        <iframe src="https://player.vimeo.com/video/341841909" width="640" height="360" frameborder="0"
+                                allow="autoplay; fullscreen" allowfullscreen></iframe>
                     </div>
                 </div>
                 <div class="card-footer d-flex flex-row justify-content-between">
