5d8f99a9f133f15280dc5ae63e4834ea0cc87372
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-06-24 11:52:57 +0200

    Migraciones

---
+++ database/migrations/2019_06_23_190749_create_files_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('path');
+            $table->string('title')->nullable();
+            $table->double('size')->default(0);
+
+            $table->bigInteger('file_upload_id')->unsigned()->index();
+            $table->foreign('file_upload_id')->references('id')->on('file_uploads')->onDelete('cascade');
+
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
