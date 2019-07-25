18416756265edc0e31642f8620746e4641ea543a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-07-26 01:03:26 +0200

    Tarjeta mejorada

--- resources/views/file_uploads/tarjeta.blade.php
+++ resources/views/file_uploads/tarjeta.blade.php
@@ -3,35 +3,43 @@
     <div class="card-body">
         <h5 class="card-title">{{ $file_upload->titulo }}</h5>
         <p class="card-text">{{ $file_upload->descripcion }}</p>
-        <form action="{{ route('uploadfile') }}" enctype="multipart/form-data" method="post">
-            @csrf
-            <div class="form-group">
-                <input type="file" name="file" id="file">
-                <input type="hidden" name="file_upload_id" value="{{ $file_upload->id }}">
-                <span class="help-block text-danger">{{ $errors->first('file') }}</span>
-            </div>
-            <button class="btn btn-primary">{{ __('Upload') }}</button>
-            <hr>
-            <table class="table table-bordered">
-                <thead class="thead-dark">
-                <tr>
-                    <th>{{ __('File') }}</th>
-                    <th>{{ __('Title') }}</th>
-                    <th>{{ __('Size') }}</th>
-                    <th>{{ __('Uploaded') }}</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach ($file_upload->files as $file)
+        @if(count($file_upload->files) > 0)
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead class="thead-dark">
                     <tr>
-                        <th><img style="width:64px" src="{{ $file->url }}"></th>
-                        <td><a href="{{ $file->url }}" target="_blank">{{ $file->title }}</a></td>
-                        <td>{{ $file->size_in_kb }} KB</td>
-                        <td>{{ $file->uploaded_time }}</td>
+                        <th>{{ __('File') }}</th>
+                        <th>{{ __('Title') }}</th>
+                        <th>{{ __('Size') }}</th>
+                        <th>{{ __('Uploaded') }}</th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </form>
+                    </thead>
+                    <tbody>
+                    @foreach ($file_upload->files as $file)
+                        <tr>
+                            <th><img style="width:64px" src="{{ $file->url }}"></th>
+                            <td><a href="{{ $file->url }}" target="_blank">{{ $file->title }}</a></td>
+                            <td>{{ $file->size_in_kb }} KB</td>
+                            <td>{{ $file->uploaded_time }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @endif
     </div>
+    @if(count($file_upload->files) < $file_upload->max_files)
+        <hr class="my-0">
+        <div class="card-body">
+            <form action="{{ route('uploadfile') }}" enctype="multipart/form-data" method="post">
+                @csrf
+                <div class="form-group">
+                    <input type="file" name="file" id="file">
+                    <input type="hidden" name="file_upload_id" value="{{ $file_upload->id }}">
+                    <span class="help-block text-danger">{{ $errors->first('file') }}</span>
+                </div>
+                <button class="btn btn-primary">{{ __('Upload') }}</button>
+            </form>
+        </div>
+    @endif
 </div>
