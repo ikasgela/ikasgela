66eaf09bb90403e60a6256caa2a52212174dfb26
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-08-27 13:31:46 +0200

    Estructura de calificaciones

--- database/seeds/QualificationsTableSeeder.php
+++ database/seeds/QualificationsTableSeeder.php
@@ -16,7 +16,11 @@ class QualificationsTableSeeder extends Seeder
      */
     public function run()
     {
-        $organization = Organization::where('slug', 'ikasgela')->first();
+        // Deusto
+
+        $organization = Organization::where('slug', 'deusto')->first();
+
+        // Competencias
 
         $ce1 = factory(Skill::class)->create([
             'organization_id' => $organization->id,
@@ -36,6 +40,8 @@ class QualificationsTableSeeder extends Seeder
             'description' => 'Conocer y dominar estructuras básicas fundamentales utilizadas en la programación, tanto estructuras de datos como estructuras de control del flujo del programa.',
         ]);
 
+        // Cualificación
+
         $cualificacion = factory(Qualification::class)->create([
             'organization_id' => $organization->id,
             'name' => 'General',
@@ -47,20 +53,78 @@ class QualificationsTableSeeder extends Seeder
         $cualificacion->skills()->attach($ce2, ['percentage' => 40]);
         $cualificacion->skills()->attach($ce3, ['percentage' => 40]);
 
-        $curso = Curso::find(1);
+        // Asociar la cualificación al curso
+
+        $curso = Curso::whereHas('category.period.organization', function ($query) {
+            $query->where('organizations.slug', 'deusto');
+        })
+            ->where('slug', 'programacion-i')
+            ->first();
+
         $curso->qualification()->associate($cualificacion);
         $curso->save();
 
+        // Ikasgela
+
+        $organization = Organization::where('slug', 'ikasgela')->first();
+
+        // Curso
+
         $cualificacion = factory(Qualification::class)->create([
             'organization_id' => $organization->id,
-            'name' => 'Unidad 2 - Diseño de algoritmos',
+            'name' => 'General',
+            'description' => 'Cualificación predeterminada para el curso.',
+            'template' => true,
+        ]);
+
+        $ud1 = factory(Skill::class)->create([
+            'organization_id' => $organization->id,
+            'name' => 'UD1 - Introducción',
+            'description' => 'Presentación del curso, conceptos básicos y uso de las herramientas.',
+        ]);
+
+        $ud2 = factory(Skill::class)->create([
+            'organization_id' => $organization->id,
+            'name' => 'UD2 - Programación estructurada',
+            'description' => 'Desarrollo de aplicaciones mediante programación estructurada.',
+        ]);
+
+        $ud3 = factory(Skill::class)->create([
+            'organization_id' => $organization->id,
+            'name' => 'UD3 - Programación modular',
+            'description' => 'Desarrollo de aplicaciones mediante programación modular.',
+        ]);
+
+        $cualificacion->skills()->attach($ud1, ['percentage' => 100]);
+        $cualificacion->skills()->attach($ud2, ['percentage' => 100]);
+        $cualificacion->skills()->attach($ud3, ['percentage' => 100]);
+
+        $curso = Curso::whereHas('category.period.organization', function ($query) {
+            $query->where('organizations.slug', 'ikasgela');
+        })
+            ->where('slug', 'programacion')
+            ->first();
+
+        $curso->qualification()->associate($cualificacion);
+        $curso->save();
+
+        // Unidad
+
+        $cualificacion = factory(Qualification::class)->create([
+            'organization_id' => $organization->id,
+            'name' => 'Colecciones',
             'description' => 'Cualificación para una unidad didáctica.',
             'template' => true,
         ]);
 
-        $cualificacion->skills()->attach($ce1, ['percentage' => 100]);
+        $cualificacion->skills()->attach($ud2, ['percentage' => 100]);
 
-        $unidad = Unidad::find(2);
+        $unidad = Unidad::whereHas('curso.category.period.organization', function ($query) {
+            $query->where('organizations.slug', 'ikasgela');
+        })
+            ->where('slug', 'gui')
+            ->first();
+
         $unidad->qualification()->associate($cualificacion);
         $unidad->save();
     }
