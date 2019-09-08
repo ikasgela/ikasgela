3415d2fb94aa1a13d6dca01097bb1bd5bbd40113
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-09-08 18:23:54 +0200

    Valor por defecto a la fecha

--- database/migrations/2019_09_08_172440_add_last_active_to_users_table.php
+++ database/migrations/2019_09_08_172440_add_last_active_to_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddLastActiveToUsersTable extends Migration
 {
@@ -14,7 +15,7 @@ class AddLastActiveToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dateTimeTz('last_active')->nullable();
+            $table->dateTimeTz('last_active')->default(Carbon::now());
         });
     }
 
