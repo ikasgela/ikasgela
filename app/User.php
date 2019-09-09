c0f62d3b6d4fdc29dd516dee78d0a3ffe395fa28
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-09-09 16:31:00 +0200

    Migración

--- app/User.php
+++ app/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'username', 'tutorial', 'last_active'
+        'name', 'email', 'password', 'username', 'tutorial', 'last_active', 'blocked_date'
     ];
 
     /**
