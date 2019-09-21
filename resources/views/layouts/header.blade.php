bf644653ddb4d87bdeec63cec8e7ca888a046c2b
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-09-21 15:48:34 +0200

    Barra de navegación con tema oscuro

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<header class="app-header navbar p-0 {{ config('app.debug') ? 'bg-warning' : 'bg-primary' }}">
+<header class="app-header navbar p-0 {{ config('app.debug') ? 'bg-warning' : 'bg-primary navbar-dark' }}">
     <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
         <span class="navbar-toggler-icon"></span>
     </button>
