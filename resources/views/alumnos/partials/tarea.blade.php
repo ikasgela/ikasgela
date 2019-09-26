020db068184a39379e5f1f05fd4807689eb9a548
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-09-26 17:37:33 +0200

    Botón de reabrir menos destacado

--- resources/views/alumnos/partials/tarea.blade.php
+++ resources/views/alumnos/partials/tarea.blade.php
@@ -83,7 +83,7 @@
                             @else
                                 <button type="submit" name="nuevoestado" value="32"
                                         onclick="return confirm('{{ __('Are you sure?') }}\n\n{{ __('Reopening the activity cancels the submission and allows making corrections, but it has a 5 point penalty.') }}')"
-                                        class="btn btn-warning">{{ __('Reopen activity') }}</button>
+                                        class="btn btn-secondary">{{ __('Reopen activity') }}</button>
                             @endif
                             @if(config('app.debug'))
                                 <button type="submit" name="nuevoestado" value="40"
