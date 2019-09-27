5fb602685886304407c54240f66e3278951806dc
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-09-27 10:47:26 +0200

    Archivo paginado con 25 filas

--- app/Http/Controllers/ArchivoController.php
+++ app/Http/Controllers/ArchivoController.php
@@ -27,7 +27,7 @@ class ArchivoController extends Controller
             session()->forget('filtrar_user_actual');
         }
 
-        $actividades = $user->actividades_archivadas()->paginate(10);
+        $actividades = $user->actividades_archivadas()->paginate(25);
 
         // Recuento de asignadas
         $num_actividades = $user->actividades_asignadas()->count();
