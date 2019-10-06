c528fd5f11129c205abd63f0219f58e5a851a025
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-10-06 18:34:26 +0200

    Color verde en los que no tienen tarea

--- resources/views/profesor/partials/tabla_usuarios.blade.php
+++ resources/views/profesor/partials/tabla_usuarios.blade.php
@@ -27,7 +27,7 @@
             <tr class="table-cell-click" data-href="{{ route('profesor.tareas', [$user->id]) }}">
                 <td class="p-0 pl-1
                     @if($user->actividades_en_curso()->count() == 0)
-                    bg-secondary
+                    bg-success
                     @elseif($user->actividades_enviadas()->count() > 0)
                     bg-danger
                     @endif
