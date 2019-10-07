db88e01612f23b001dc6465dc9118d39405d2c39
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-10-07 09:27:55 +0200

    Barra de botones más visible

--- resources/views/profesor/index.blade.php
+++ resources/views/profesor/index.blade.php
@@ -7,20 +7,30 @@
             @include('partials.boton_recargar')
         </h1>
         <div class="form-inline">
-            {!! Form::open(['route' => ['profesor.index'], 'method' => 'POST']) !!}
-            {!! Form::button(__('Alphabetic order'), ['type' => 'submit', 'class' => 'btn btn-link text-secondary']) !!}
-            {!! Form::hidden('filtro_alumnos','A') !!}
-            {!! Form::close() !!}
+            <div class="btn-toolbar" role="toolbar">
+                {!! Form::open(['route' => ['profesor.index'], 'method' => 'POST']) !!}
 
-            {!! Form::open(['route' => ['profesor.index'], 'method' => 'POST']) !!}
-            {!! Form::button(__('Pending review'), ['type' => 'submit', 'class' => 'btn btn-link text-secondary']) !!}
-            {!! Form::hidden('filtro_alumnos','R') !!}
-            {!! Form::close() !!}
+                {!! Form::button(__('Alphabetic order'), ['type' => 'submit',
+                    'class' => session('profesor_filtro_alumnos') == 'A' ? 'btn btn-sm mx-1 btn-secondary' : 'btn btn-sm mx-1 btn-outline-secondary'
+                ]) !!}
+                {!! Form::hidden('filtro_alumnos','A') !!}
+                {!! Form::close() !!}
 
-            {!! Form::open(['route' => ['profesor.index'], 'method' => 'POST']) !!}
-            {!! Form::button(__('Progress'), ['type' => 'submit', 'class' => 'btn btn-link text-secondary']) !!}
-            {!! Form::hidden('filtro_alumnos','P') !!}
-            {!! Form::close() !!}
+                {!! Form::open(['route' => ['profesor.index'], 'method' => 'POST']) !!}
+                {!! Form::button(__('Pending review'), ['type' => 'submit',
+                    'class' => session('profesor_filtro_alumnos') == 'R' ? 'btn btn-sm mx-1 btn-secondary' : 'btn btn-sm mx-1 btn-outline-secondary'
+                ]) !!}
+                {!! Form::hidden('filtro_alumnos','R') !!}
+                {!! Form::close() !!}
+
+                {!! Form::open(['route' => ['profesor.index'], 'method' => 'POST']) !!}
+                {!! Form::button(__('Progress'), ['type' => 'submit',
+                    'class' => session('profesor_filtro_alumnos') == 'P' ? 'btn btn-sm mx-1 btn-secondary' : 'btn btn-sm mx-1 btn-outline-secondary'
+                ]) !!}
+                {!! Form::hidden('filtro_alumnos','P') !!}
+
+                {!! Form::close() !!}
+            </div>
         </div>
         <h2 class="text-muted font-xl">{{ $organization->name ?? '?' }}</h2>
     </div>
