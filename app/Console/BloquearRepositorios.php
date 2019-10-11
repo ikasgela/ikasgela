8c5d7eacceafe5885674dec7988056e775f10414
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-10-11 12:22:51 +0200

    Bloquear automáticamente cada 5 minutos

--- app/Console/BloquearRepositorios.php
+++ app/Console/BloquearRepositorios.php
@@ -10,15 +10,20 @@ class BloquearRepositorios
 {
     public function __invoke()
     {
-        $actividades = Actividad::where('fecha_limite', '<=', Carbon::now())->get();
+        $actividades = Actividad::where('plantilla', false)->where('fecha_limite', '<=', Carbon::now())->get();
+
+        $total = 0;
 
         foreach ($actividades as $actividad) {
             foreach ($actividad->intellij_projects as $intellij_project) {
                 $proyecto_gitlab = $intellij_project->gitlab();
                 if (!$proyecto_gitlab['archived']) {
                     GitLab::projects()->archive($proyecto_gitlab['id']);
+                    $total += 1;
                 }
             }
         }
+
+        echo 'Repositorios archivados: ' . $total;
     }
 }
