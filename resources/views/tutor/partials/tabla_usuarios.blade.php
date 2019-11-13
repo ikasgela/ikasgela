aa2decd67b93b4a36a119090acd60df43701a932
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-11-13 11:46:02 +0100

    Colorear los problemas

--- resources/views/tutor/partials/tabla_usuarios.blade.php
+++ resources/views/tutor/partials/tabla_usuarios.blade.php
@@ -22,10 +22,16 @@
                     @include('profesor.partials.status_usuario')
                 </td>
                 @foreach($unidades as $unidad)
-                    <td class="text-center">{{ $resultados_usuario_unidades[$user->id][$unidad->id]->tarea + 0
-                    }}/{{ $resultados_usuario_unidades[$user->id][$unidad->id]->actividad + 0 }}</td>
+                    @php($porcentaje = $resultados_usuario_unidades[$user->id][$unidad->id]->actividad > 0
+                    ? ($resultados_usuario_unidades[$user->id][$unidad->id]->tarea/$resultados_usuario_unidades[$user->id][$unidad->id]->actividad*100) : 0)
+                    @if($resultados_usuario_unidades[$user->id][$unidad->id]->actividad > 0)
+                        <td class="text-center {{ $porcentaje<50 ? 'bg-warning text-dark' : '' }}">
+                            @if($porcentaje>0){{ $porcentaje }} %@endif</td>
+                    @else
+                        <td></td>
+                    @endif
                 @endforeach
-                <td class="clickable text-center">{{ $user->actividades_completadas()->count() }}</td>
+                <td class="clickable text-center {{ $user->actividades_completadas()->count() < $total_actividades_grupo / $usuarios->count() ? 'bg-warning text-dark' : '' }}">{{ $user->actividades_completadas()->count() }}</td>
                 <td class="clickable">{{ $user->last_active_time }}</td>
             </tr>
         @endforeach
