66241947767993d089b8931a6e788fa48b4a2a18
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-11-17 18:01:33 +0100

    Añadir etiquetas a las unidades

---
+++ database/migrations/2019_11_17_175945_add_tags_to_unidades_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTagsToUnidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('unidades', function (Blueprint $table) {
+            $table->string('tags')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('unidades', function (Blueprint $table) {
+            $table->dropColumn('tags');
+        });
+    }
+}
