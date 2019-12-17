92f8eecfc73f1aea4745ad569e70511966f2f7a1
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-12-17 02:19:18 +0100

    Generar todos los días y mezclar los datos

--- app/Http/Controllers/TutorController.php
+++ app/Http/Controllers/TutorController.php
@@ -8,6 +8,7 @@ use App\Registro;
 use App\Tarea;
 use App\Traits\InformeGrupo;
 use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -58,17 +59,24 @@ class TutorController extends Controller
             ->orderBy('timestamp')
             ->get()
             ->groupBy(function ($val) {
-                return Carbon::parse($val->timestamp)->format('Y-m-d');
+                return Carbon::parse($val->timestamp)->format('d/m/Y');
             });
 
-        $chart->labels($registros->keys()->map(function ($item, $key) {
-            return Carbon::parse($item)->format('d/m/Y');
-        }))->displayLegend(false);
+        $period = CarbonPeriod::create('2019-09-01', Carbon::now());
+
+        $todas_fechas = [];
+        foreach ($period as $date) {
+            $todas_fechas[$date->format('d/m/Y')] = 0;
+        }
+
+        $datos = array_merge($todas_fechas, $registros->map(function ($item, $key) {
+            return $item->count();
+        })->toArray());
+
+        $chart->labels(array_keys($datos))->displayLegend(false);
 
         $chart->dataset('Enviadas', 'bar',
-            $registros->map(function ($item, $key) {
-                return $item->count();
-            })->values())
+            array_values($datos))
             ->color("#3490dc")
             ->backgroundColor("#d6e9f8");
 
