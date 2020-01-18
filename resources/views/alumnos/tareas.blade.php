133bf2156cf5b5095ce3ffc8d68c89daeccb8c2e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-01-18 11:15:52 +0100

    Recuento de en curso y enviadas

--- resources/views/alumnos/tareas.blade.php
+++ resources/views/alumnos/tareas.blade.php
@@ -22,11 +22,20 @@
     <ul class="nav nav-tabs" id="pills-tab" role="tablist">
         <li class="nav-item">
             <a class="nav-link active" id="pills-en-curso-tab" data-toggle="tab" href="#pills-en-curso" role="tab"
-               aria-controls="pills-profile" aria-selected="true">En curso</a>
+               aria-controls="pills-profile" aria-selected="true">En curso
+                @if($user->actividades_en_curso_autoavance()->count() > 0)
+                    <span class="ml-2 badge badge-danger">{{ $user->actividades_en_curso_autoavance()->count() }}</span>
+                @else
+                    <span class="ml-2 badge badge-secondary">0</span>
+                @endif
+            </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" id="pills-enviadas-tab" data-toggle="tab" href="#pills-enviadas" role="tab"
-               aria-controls="pills-contact" aria-selected="false">Enviadas</a>
+               aria-controls="pills-contact" aria-selected="false">Enviadas
+                <span
+                    class="ml-2 badge badge-secondary">{{ $user->actividades_enviadas_noautoavance()->count() }}</span>
+            </a>
         </li>
     </ul>
     <div class="tab-content" id="pills-tab-content">
