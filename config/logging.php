4b54154b5ec4adb90260981c4a62d1682ec955bf
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-02-09 18:50:02 +0100

    Registro de eventos de Laravel en Graylog

--- config/logging.php
+++ config/logging.php
@@ -1,5 +1,7 @@
 <?php
 
+use Hedii\LaravelGelfLogger\GelfLoggerFactory;
+use Hedii\LaravelGelfLogger\Processors\NullStringProcessor;
 use Monolog\Handler\StreamHandler;
 
 return [
@@ -35,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'gelf'],
         ],
 
         'single' => [
@@ -75,6 +77,54 @@ return [
         'errorlog' => [
             'driver' => 'errorlog',
             'level' => 'debug',
+        ],
+
+        'gelf' => [
+            'driver' => 'custom',
+
+            'via' => GelfLoggerFactory::class,
+
+            // This optional option determines the processors that should be
+            // pushed to the handler. This option is useful to modify a field
+            // in the log context (see NullStringProcessor), or to add extra
+            // data. Each processor must be a callable or an object with an
+            // __invoke method: see monolog documentation about processors.
+            // Default is an empty array.
+            'processors' => [
+                NullStringProcessor::class,
+            ],
+
+            // This optional option determines the minimum "level" a message
+            // must be in order to be logged by the channel. Default is 'debug'
+            'level' => 'debug',
+
+            // This optional option determines the channel name sent with the
+            // message in the 'facility' field. Default is equal to app.env
+            // configuration value
+            'name' => 'laravel',
+
+            // This optional option determines the system name sent with the
+            // message in the 'source' field. When forgotten or set to null,
+            // the current hostname is used.
+            'system_name' => null,
+
+            // This optional option determines if you want the TCP transport
+            // for the gelf log messages. Default is UDP
+            'transport' => 'udp',
+
+            // This optional option determines the host that will receive the
+            // gelf log messages. Default is 127.0.0.1
+            'host' => 'graylog',
+
+            // This optional option determines the port on which the gelf
+            // receiver host is listening. Default is 12201
+            'port' => 12201,
+
+            // This optional option determines the maximum length per message
+            // field. When forgotten or set to null, the default value of
+            // \Monolog\Formatter\GelfMessageFormatter::DEFAULT_MAX_LENGTH is
+            // used (currently this value is 32766)
+            'max_length' => null,
         ],
     ],
 
