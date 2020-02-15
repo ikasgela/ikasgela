338e45776190b578ad6a46c39db67a726bfa1be3
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-02-15 16:44:31 +0100

    Crear el usuario de Gitea

--- config/ikasgela.php
+++ config/ikasgela.php
@@ -4,5 +4,7 @@ return [
 
     'repo_cache_days' => env('REPO_CACHE_DAYS', 7),
     'markdown_cache_days' => env('MARKDOWN_CACHE_DAYS', 7),
+    'gitlab_enabled' => env('GITLAB_ENABLED', false),
+    'gitea_enabled' => env('GITEA_ENABLED', false),
 
 ];
