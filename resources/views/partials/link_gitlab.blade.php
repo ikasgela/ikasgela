5ecc0a41684e04d854715ef647bcee42cd203d5e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-03-19 17:48:35 +0100

    Clonador mejorado

--- resources/views/partials/link_gitlab.blade.php
+++ resources/views/partials/link_gitlab.blade.php
@@ -1,5 +1,5 @@
 @isset($proyecto['http_url_to_repo'])
     <a target="_blank" href="{{ $proyecto['http_url_to_repo'] }}">{{ $proyecto['path_with_namespace'] }}</a>
 @else
-    <a target="_blank" href="{{ $proyecto['html_url'] }}">{{ $proyecto['name'] }}</a>
+    <a target="_blank" href="{{ $proyecto['html_url'] }}">{{ $proyecto['full_name'] }}</a>
 @endisset
