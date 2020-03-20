3487bf773a096375ebad9f5eb951301cf3c6e4b4
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-03-20 19:10:49 +0100

    Imprimir correctamente

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="c-footer c-footer-fixed align-middle d-flex justify-content-center">
+<footer class="c-footer align-middle d-flex justify-content-center">
     <span>&copy; {{ \Carbon\Carbon::now()->year }} Ion Jaureguialzo Sarasola</span>
 </footer>
