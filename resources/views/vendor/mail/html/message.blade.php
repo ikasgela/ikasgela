2307899000c8ac08da609730ebfbf4cb2ec10f68
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-03-21 17:02:50 +0100

    Logotipo en los mensajes

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
 {{-- Header --}}
 @slot('header')
 @component('mail::header', ['url' => config('app.url')])
-{{ config('app.name') }}
+<img src="{{ url('/svg/logo-black.svg') }}" class="logo" alt="ikasgela">
 @endcomponent
 @endslot
 
