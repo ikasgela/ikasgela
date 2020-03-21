fddd3525c1c36c86b963160a900e7b88db83be9e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-03-21 02:14:48 +0100

    Link al / en la barra lateral

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,10 @@
 <div id="sidebar" class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show">
     <div class="c-sidebar-brand {{ config('app.debug') ? 'bg-warning' : 'bg-primary c-header-dark' }}">
-        <img class="c-sidebar-brand-full c-d-dark-none"
-             src="{{ config('app.debug') ? url('/svg/logo-black.svg') : url('/svg/logo-dark.svg') }}" height="25"
-             alt="{{ __('Logo') }}">
+        <a href="{{ url('/') }}">
+            <img class="c-sidebar-brand-full c-d-dark-none"
+                 src="{{ config('app.debug') ? url('/svg/logo-black.svg') : url('/svg/logo-dark.svg') }}" height="25"
+                 alt="{{ __('Logo') }}">
+        </a>
     </div>
     <ul class="c-sidebar-nav">
         @auth
