9accc5c9f1ab53742fd4f883056249dc216e32d0
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-03-22 12:42:24 +0100

    Plugin codesample activado

--- resources/views/messenger/partials/tinymce.blade.php
+++ resources/views/messenger/partials/tinymce.blade.php
@@ -3,9 +3,18 @@
     tinymce.init({
         selector: 'textarea#message',
         language: 'es_ES',
-        plugins: "link image autolink emoticons lists hr",
+        plugins: "link image autolink emoticons lists hr codesample",
         default_link_target: "_blank",
-        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | image link emoticons hr',
-        link_assume_external_targets: true
+        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | codesample | image link emoticons hr',
+        link_assume_external_targets: true,
+        codesample_languages: [
+            {text: 'Java', value: 'java'},
+            {text: 'HTML/XML', value: 'markup'},
+            {text: 'CSS', value: 'css'},
+            {text: 'JavaScript', value: 'javascript'},
+            {text: 'PHP', value: 'php'},
+            {text: 'Python', value: 'python'},
+            {text: 'C#', value: 'csharp'},
+        ],
     });
 </script>
