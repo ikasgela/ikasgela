3bc89377e3805f4fa9163b0f8072c6d26bdf95b6
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-04-04 19:25:34 +0200

    Memorizar rutas mejorado

--- app/Helpers/MemorizarRuta.php
+++ app/Helpers/MemorizarRuta.php
@@ -4,11 +4,29 @@ if (!function_exists('memorizar_ruta')) {
 
     function memorizar_ruta()
     {
-        session(['_ruta_actual' => url()->full()]);
+        // REF: https://stackoverflow.com/a/36098635/5136913
+
+        $accion = Route::getCurrentRoute()->getActionMethod();
+
+        if ($accion == 'index')
+            $rutas = [];
+        else
+            $rutas = session()->has('_rutas') ? session('_rutas') : [];
+
+        $actual = request()->path();
+
+        array_unshift($rutas, $actual);
+
+        session(['_rutas' => $rutas]);
     }
 
-    function ruta_memorizada()
+    function ruta_memorizada(int $niveles = 1)
     {
-        return session('_ruta_actual') ?: url()->previous();
+        return session('_rutas')[$niveles];
+    }
+
+    function anterior(int $niveles = 1)
+    {
+        return url(ruta_memorizada($niveles));
     }
 }
