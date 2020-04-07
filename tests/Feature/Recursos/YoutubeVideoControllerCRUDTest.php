803465a5e6b1b5468747565341b555e891ac60f5
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-04-07 19:48:47 +0200

    Renombrado

--- tests/Feature/Recursos/YoutubeVideoControllerCRUDTest.php
+++ tests/Feature/Recursos/YoutubeVideoControllerCRUDTest.php
@@ -6,7 +6,7 @@ use App\YoutubeVideo;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
-class YoutubeVideoControllerTest extends TestCase
+class YoutubeVideoControllerCRUDTest extends TestCase
 {
     use DatabaseTransactions;
 
