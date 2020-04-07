df6da5c065f0622d59e4cb66082b44242a30ede4
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-04-07 20:26:07 +0200

    Test de asociar y desasociar vídeos

--- tests/Feature/Recursos/YoutubeVideoControllerExtraTest.php
+++ tests/Feature/Recursos/YoutubeVideoControllerExtraTest.php
@@ -44,15 +44,52 @@ class YoutubeVideoControllerExtraTest extends TestCase
         ]);
     }
 
-    /*    public function testAsociar()
-        {
-        }
+    public function testAsociar()
+    {
+        // Given
+        $this->actingAs($this->profesor);
 
-        public function testDesasociar()
-        {
-            // Given
-            // When
-            // Then
-        }*/
+        $actividad = factory(Actividad::class)->create();
+        $youtube_video1 = factory(YoutubeVideo::class)->create();
+        $youtube_video2 = factory(YoutubeVideo::class)->create();
 
+        // When
+        $this->post(route('youtube_videos.asociar', $actividad), ['seleccionadas' => [$youtube_video1, $youtube_video2]]);
+
+        // Then
+        $this->assertCount(2, $actividad->youtube_videos()->get());
+    }
+
+    public function testAsociarRequiresSeleccionadas()
+    {
+        // Given
+        $this->actingAs($this->profesor);
+
+        $actividad = factory(Actividad::class)->create();
+
+        // When
+        $response = $this->post(route('youtube_videos.asociar', $actividad), ['seleccionadas' => null]);
+
+        // Then
+        $response->assertSessionHasErrors('seleccionadas');
+    }
+
+    public function testDesasociar()
+    {
+        // Given
+        $this->actingAs($this->profesor);
+
+        $actividad = factory(Actividad::class)->create();
+        $youtube_video1 = factory(YoutubeVideo::class)->create();
+        $youtube_video2 = factory(YoutubeVideo::class)->create();
+
+        $actividad->youtube_videos()->attach($youtube_video1);
+        $actividad->youtube_videos()->attach($youtube_video2);
+
+        // When
+        $this->delete(route('youtube_videos.desasociar', [$actividad, $youtube_video1]));
+
+        // Then
+        $this->assertCount(1, $actividad->youtube_videos()->get());
+    }
 }
