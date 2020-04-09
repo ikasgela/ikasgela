b1c8dbbd5a9484c2d67c8d8e625710f622f8d313
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-04-09 17:00:00 +0200

    Test unitario para un modelo
    https://dev.to/tonyfrenzy/part-1-testing-model-relationships-in-laravel-schema-tests-37nj

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function users_table_has_expected_columns()
+    {
+        $this->assertTrue(
+            Schema::hasColumns('users', [
+                'id', 'name', 'email', 'username', 'email_verified_at',
+                'password', 'remember_token', 'tutorial', 'created_at', 'updated_at',
+                'last_active', 'blocked_date', 'max_simultaneas', 'tags',
+            ]), 1);
+    }
+}
