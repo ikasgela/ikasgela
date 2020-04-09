6544fcbbe34a93972ccf26bb511fc2d3e0f5849b
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-04-09 11:25:13 +0200

    Test para el middleware de usuario bloqueado

---
+++ tests/Feature/Usuarios/CheckBlockedTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature\Usuarios;
+
+use Tests\TestCase;
+
+class CheckBlockedTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        parent::crearUsuarios();
+    }
+
+    /** @test */
+    public function unblocked_user_can_login()
+    {
+        // Auth
+        $this->actingAs($this->alumno);
+
+        // Given
+        $this->alumno->blocked_date = null;
+
+        // When
+        $response = $this->get(route('users.home'));
+
+        // Then
+        $response->assertSee(__('Desktop'));
+    }
+
+    /** @test */
+    public function blocked_user_cannot_login()
+    {
+        // Auth
+        $this->actingAs($this->alumno);
+
+        // Given
+        $this->alumno->blocked_date = now()->addDays(-1);
+
+        // When
+        $response = $this->get(route('users.home'));
+
+        // Then
+        $response->assertRedirect(route('login'));
+    }
+}
