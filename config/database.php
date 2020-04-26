c9dd8f69e8866c3091ec42cfe83df820ffa2e5ce
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-04-26 16:36:48 +0200

    Soporte para MongoDB en Laravel

--- config/database.php
+++ config/database.php
@@ -78,6 +78,17 @@ return [
             'prefix' => '',
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('MONGODB_HOST', 'localhost'),
+            'port' => env('MONGODB_PORT', 27017),
+            'database' => env('MONGODB_DATABASE'),
+            'username' => env('MONGODB_USERNAME'),
+            'password' => env('MONGODB_PASSWORD'),
+            'options' => [
+                'database' => env('MONGODB_DATABASE') // sets the authentication database required by mongo 3
+            ]
+        ],
     ],
 
     /*
