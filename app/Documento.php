37bb3279e44661642bec22aed4f05775171dce5c
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-05-02 13:24:58 +0200

    Insertar registros

--- app/Documento.php
+++ app/Documento.php
@@ -7,4 +7,10 @@ use Jenssegers\Mongodb\Eloquent\Model as MongoModel;
 class Documento extends MongoModel
 {
     protected $connection = 'mongodb';
+
+//    protected $fillable = [
+//        'title', 'description'
+//    ];
+
+    protected $guarded = ['_id'];
 }
--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -14,7 +14,17 @@ class DocumentoController extends Controller
 
     public function create()
     {
-        //
+        $documentos = json_decode('[
+            { "title" : "Prueba1" },
+            { "title" : "Prueba2", "description": [1,2,3] },
+            { "title" : "Prueba3" }
+        ]', true);
+
+        foreach ($documentos as $documento) {
+            Documento::create($documento);
+        }
+
+        return redirect(route('documentos.index'));
     }
 
     public function store(Request $request)
