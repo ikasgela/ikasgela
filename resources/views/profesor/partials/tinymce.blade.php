b3ee5843a144f47b8bfeb6964e3c8873615e2bd4
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-05-23 11:13:37 +0200

    Soporte para más lenguajes en el editor

--- resources/views/profesor/partials/tinymce.blade.php
+++ resources/views/profesor/partials/tinymce.blade.php
@@ -15,6 +15,9 @@
             {text: 'Java', value: 'java'},
             {text: 'Swift', value: 'swift'},
             {text: 'Python', value: 'python'},
+            {text: 'HTML/XML', value: 'markup'},
+            {text: 'PHP', value: 'php'},
+            {text: 'SQL', value: 'sql'},
         ],
         setup: function (editor) {
             editor.on('init', function () {
