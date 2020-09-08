ccfa26f9f5d514ad2e2e0ab4517e82cdf61aab9e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-09-08 17:34:30 +0200

    Editor TinyMCE para el feedback

--- resources/views/feedbacks/edit.blade.php
+++ resources/views/feedbacks/edit.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('tinymce')
+    @include('feedbacks.partials.tinymce')
+@endsection
+
 @section('content')
 
     @include('partials.titular', ['titular' => __('Edit feedback message')])
@@ -15,7 +19,14 @@
                 }}
             {{ Form::hidden('curso_id',$feedback->curso_id) }}
             {{ Form::campoTexto('titulo', __('Title')) }}
-            {{ Form::campoTexto('mensaje', __('Message')) }}
+
+            <div class="form-group row">
+                {!! Form::label('mensaje', __('Message'), ['class' => 'col-sm-2 col-form-label']) !!}
+                <div class="col-sm-10">
+                    <textarea rows="10" class="form-control" id="mensaje"
+                              name="mensaje">{!! $feedback->mensaje !!}</textarea>
+                </div>
+            </div>
 
             @include('partials.guardar_cancelar')
 
