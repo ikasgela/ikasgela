d7442038960a15d2114a27ad1ddc16d686d9ffd6
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-09-09 09:02:12 +0200

    Mostrar el periodo actual en la tabla de organizaciones

--- resources/views/organizations/index.blade.php
+++ resources/views/organizations/index.blade.php
@@ -16,6 +16,7 @@
                 <th>{{ __('Name') }}</th>
                 <th>{{ __('Slug') }}</th>
                 <th>{{ __('Registration open') }}</th>
+                <th>{{ __('Current period') }}</th>
                 <th>{{ __('Actions') }}</th>
             </tr>
             </thead>
@@ -26,6 +27,7 @@
                     <td>{{ $organization->name }}</td>
                     <td>{{ $organization->slug }}</td>
                     <td>{{ $organization->registration_open ? __('Yes') : __('No') }}</td>
+                    <td>{{ $organization->current_period()->name ?? '' }}</td>
                     <td>
                         {!! Form::open(['route' => ['organizations.destroy', $organization->id], 'method' => 'DELETE']) !!}
                         <div class='btn-group'>
