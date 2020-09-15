038d42147697295672d3f55949560ff444f3f424
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-09-16 00:01:48 +0200

    Mostrar las etiquetas en el CRUD

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -24,7 +24,10 @@
                     <td>{{ $user->id }}</td>
                     <td><img style="height:35px;" src="{{ $user->avatar_url(70) }}"
                              onerror="this.onerror=null;this.src='{{ url("/svg/missing_avatar.svg") }}';"/></td>
-                    <td>{{ $user->name }}</td>
+                    <td>
+                        {{ $user->name }}
+                        @include('profesor.partials.status_usuario')
+                    </td>
                     <td><a href="mailto:{{ $user->email }}">{{ $user->email }}</a></td>
                     <td class="text-center">{!! $user->hasVerifiedEmail() ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-times text-danger"></i>' !!}</td>
                     <td class="text-center">{!! $user->tutorial ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-times text-danger"></i>' !!}</td>
