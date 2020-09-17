f8e6d1c7eaeef40710b711094d4591a9ee7a799d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-09-17 09:53:23 +0200

    Avanza

--- app/Http/Controllers/Resultado.php
+++ app/Http/Controllers/Resultado.php
@@ -7,4 +7,12 @@ class Resultado
     public $actividad;
     public $tarea;
     public $porcentaje;
+
+    public $puntos_tarea;
+    public $puntos_totales_tarea;
+    public $puntos_examen;
+    public $puntos_totales_examen;
+
+
+    public $puntos_totales;
 }
