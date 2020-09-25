e278ebb29a2d9b2204e066f7ba7d8b60859c6c15
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-09-25 08:41:48 +0200

    Ampliar el TinyMCE de edición a 25 líneas

--- resources/views/feedbacks/create.blade.php
+++ resources/views/feedbacks/create.blade.php
@@ -32,7 +32,7 @@
             <div class="form-group row">
                 {!! Form::label('mensaje', __('Message'), ['class' => 'col-sm-2 col-form-label']) !!}
                 <div class="col-sm-10">
-                    <textarea rows="10" class="form-control" id="mensaje"
+                    <textarea rows="25" class="form-control" id="mensaje"
                               name="mensaje"></textarea>
                 </div>
             </div>
