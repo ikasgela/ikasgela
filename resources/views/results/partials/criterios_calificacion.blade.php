0f39242f2fbcdb0bbf447dbf9a70216ce0ac6847
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-12-10 11:36:11 +0100

    Criterios de calificación

---
+++ resources/views/results/partials/criterios_calificacion.blade.php
@@ -0,0 +1,16 @@
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Criterios de calificación aplicados</h5>
+        <ul class="card-text mb-1">
+            <li>Se debe superar el 50% en cada una de las competencias de forma individual.
+                <i class="fas fa-check text-success"></i><i class="fas fa-times text-danger"></i>
+            </li>
+            <li>Se deben haber realizado al menos el 80% de las actividades propuestas.
+                <i class="fas fa-check text-success"></i><i class="fas fa-times text-danger"></i>
+            </li>
+            <li>Se deben haber superado todas las pruebas teórico-prácticas individuales de cada competencia.
+                <i class="fas fa-check text-success"></i><i class="fas fa-times text-danger"></i>
+            </li>
+        </ul>
+    </div>
+</div>
