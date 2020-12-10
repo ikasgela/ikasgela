b4747dbb0998ed8ff9a72ba59f22ae6d2add6c1b
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-12-10 12:18:17 +0100

    Mostrar el criterio del 80%

--- resources/views/results/partials/criterios_calificacion.blade.php
+++ resources/views/results/partials/criterios_calificacion.blade.php
@@ -2,10 +2,18 @@
     <div class="card-body">
         <h5 class="card-title">Criterios de calificación aplicados</h5>
         <ul class="card-text mb-1">
+            @if($curso->minimo_entregadas > 0)
+                <li>Se deben haber realizado al menos el {{ $curso->minimo_entregadas }}% de las actividades propuestas
+                    de cada unidad.
+
+                    @if($actividades_obligatorias_superadas)
+                        <i class="fas fa-check text-success"></i>
+                    @else
+                        <i class="fas fa-times text-danger"></i>
+                    @endif
+                </li>
+            @endif
             <li>Se debe superar el 50% en cada una de las competencias de forma individual.
-                <i class="fas fa-check text-success"></i><i class="fas fa-times text-danger"></i>
-            </li>
-            <li>Se deben haber realizado al menos el 80% de las actividades propuestas.
                 <i class="fas fa-check text-success"></i><i class="fas fa-times text-danger"></i>
             </li>
             <li>Se deben haber superado todas las pruebas teórico-prácticas individuales de cada competencia.
