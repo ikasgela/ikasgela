d2287a038b6891a0ae1baffea13ab09bfd43fc5a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-12-10 19:43:35 +0100

    Revert "Reactivar el seguimiento de IPs"
    This reverts commit b4def366e9c7190cdea6d7a06be0048d015341a0.

--- app/Helpers/CloudflareHelper.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// REF: https://laracasts.com/discuss/channels/laravel/cloudflare-and-user-ip
-
-if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-    $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-}
