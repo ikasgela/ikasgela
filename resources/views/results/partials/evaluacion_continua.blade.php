4b8cd34c32c1407517648e7ef9b250e9b75c5205
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-12-11 01:02:02 +0100

    Mínimo de entregas a 0 da por superado

--- resources/views/results/partials/evaluacion_continua.blade.php
+++ resources/views/results/partials/evaluacion_continua.blade.php
@@ -1,15 +1,17 @@
 @include('partials.subtitulo', ['subtitulo' => __('Continuous evaluation')])
 
 <div class="card-deck">
-    <div
-        class="card mb-3 {{ $num_actividades_obligatorias > 0 ? $actividades_obligatorias_superadas ? 'bg-success text-white' : 'bg-warning text-dark' : 'bg-light text-dark' }}">
+    @if($curso->minimo_entregadas > 0)
         <div
-            class="card-header">{{ __('Mandatory activities') }}</div>
-        <div class="card-body text-center">
-            <p class="card-text"
-               style="font-size:150%;">{{ $num_actividades_obligatorias > 0 ? $actividades_obligatorias_superadas ? trans_choice('tasks.completed', 2) : ($numero_actividades_completadas+0)."/".($num_actividades_obligatorias+0)  : __('None') }}</p>
+            class="card mb-3 {{ $actividades_obligatorias_superadas ? 'bg-success text-white' : 'bg-warning text-dark' }}">
+            <div
+                class="card-header">{{ __('Mandatory activities') }}</div>
+            <div class="card-body text-center">
+                <p class="card-text"
+                   style="font-size:150%;">{{ $num_actividades_obligatorias > 0 ? $actividades_obligatorias_superadas ? trans_choice('tasks.completed', 2) : ($numero_actividades_completadas+0)."/".($num_actividades_obligatorias+0)  : __('None') }}</p>
+            </div>
         </div>
-    </div>
+    @endif
     @if($minimo_competencias > 0)
         <div
             class="card mb-3 {{ $competencias_50_porciento ? 'bg-success text-white' : 'bg-warning text-dark' }}">
@@ -33,13 +35,13 @@
         </div>
     @endif
     <div
-        class="card mb-3 {{ ($actividades_obligatorias_superadas || $num_actividades_obligatorias == 0)
+        class="card mb-3 {{ ($actividades_obligatorias_superadas || $num_actividades_obligatorias == 0 || $curso->minimo_entregadas == 0)
                 && (!$curso->examenes_obligatorios || $pruebas_evaluacion || $num_pruebas_evaluacion == 0)
                 && $competencias_50_porciento && $nota_final >= 5 ? 'bg-success text-white' : 'bg-warning text-dark' }}">
         <div class="card-header">{{ __('Continuous evaluation') }}</div>
         <div class="card-body text-center">
             <p class="card-text"
-               style="font-size:150%;">{{ ($actividades_obligatorias_superadas || $num_actividades_obligatorias == 0)
+               style="font-size:150%;">{{ ($actividades_obligatorias_superadas || $num_actividades_obligatorias == 0 || $curso->minimo_entregadas == 0)
                 && (!$curso->examenes_obligatorios || $pruebas_evaluacion || $num_pruebas_evaluacion == 0)
                 && $competencias_50_porciento && $nota_final >= 5 ? trans_choice('tasks.passed', 1) : trans_choice('tasks.not_passed', 1) }}</p>
         </div>
