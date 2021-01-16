23832078b78574e16ac3573e97a25a046d1da2d6
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-01-16 12:11:36 +0100

    PDF unificado

--- resources/views/results/pdf.blade.php
+++ resources/views/results/pdf.blade.php
@@ -19,73 +19,7 @@
         </tr>
     </table>
 
-    <h2>{{ __('Continuous evaluation') }}</h2>
-
-    <table class="tabla-marcador-contenedor">
-        <tr>
-            @if($curso->minimo_entregadas > 0)
-                <td>
-                    <table
-                        class="tabla-marcador {{ $calificaciones->actividades_obligatorias_superadas ? 'bg-success text-white' : 'bg-warning text-dark' }}">
-                        <tr>
-                            <th>{{ __('Mandatory activities') }}</th>
-                        </tr>
-                        <tr>
-                            <td class="text-center">{{ $calificaciones->num_actividades_obligatorias > 0 ? $calificaciones->actividades_obligatorias_superadas ? trans_choice('tasks.completed', 2) : ($calificaciones->numero_actividades_completadas+0)."/".($calificaciones->num_actividades_obligatorias+0)  : __('None') }}</td>
-                        </tr>
-                    </table>
-                </td>
-            @endif
-            @if($calificaciones->minimo_competencias > 0)
-                <td>
-                    <table
-                        class="tabla-marcador {{ $calificaciones->competencias_50_porciento ? 'bg-success text-white' : 'bg-warning text-dark' }}">
-                        <tr>
-                            <th>{{ __('Skills') }}</th>
-                        </tr>
-                        <tr>
-                            <td class="text-center">{{ $calificaciones->competencias_50_porciento ? trans_choice('tasks.passed', 2) : trans_choice('tasks.not_passed', 2) }}</td>
-                        </tr>
-                    </table>
-                </td>
-            @endif
-            @if($calificaciones->num_pruebas_evaluacion > 0)
-                <td>
-                    <table
-                        class="tabla-marcador {{ $curso->examenes_obligatorios ? $calificaciones->pruebas_evaluacion ? 'bg-success text-white' : 'bg-warning text-dark' : 'bg-light text-dark' }}">
-                        <tr>
-                            <th>{{ __('Assessment tests') }}</th>
-                        </tr>
-                        <tr>
-                            <td class="text-center">{{ $calificaciones->num_pruebas_evaluacion > 0 ? $calificaciones->pruebas_evaluacion ? trans_choice('tasks.passed', 2) : trans_choice('tasks.not_passed', 2) : __('None') }}</td>
-                        </tr>
-                    </table>
-                </td>
-            @endif
-            <td>
-                <table
-                    class="tabla-marcador {{ $calificaciones->evaluacion_continua_superada ? 'bg-success text-white' : 'bg-warning text-dark' }}">
-                    <tr>
-                        <th>{{ __('Continuous evaluation') }}</th>
-                    </tr>
-                    <tr>
-                        <td class="text-center">{{ $calificaciones->evaluacion_continua_superada ? trans_choice('tasks.passed', 1) : trans_choice('tasks.not_passed', 1) }}</td>
-                    </tr>
-                </table>
-            </td>
-            <td>
-                <table
-                    class="tabla-marcador bg-light text-dark">
-                    <tr>
-                        <th>{{ __('Calification') }}</th>
-                    </tr>
-                    <tr>
-                        <td class="text-center">{{ $calificaciones->evaluacion_continua_superada ? $calificaciones->nota_final : ($curso->disponible() ? __('Unavailable') : __('Fail')) }}</td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
+    @include('results.pdf.evaluacion_continua')
 
     @include('results.partials.criterios_calificacion')
 
---
+++ resources/views/results/pdf/evaluacion_continua.blade.php
@@ -0,0 +1,67 @@
+<h2>{{ __('Evaluation and calification') }}</h2>
+
+<table class="tabla-marcador-contenedor">
+    <tr>
+        @if($curso->minimo_entregadas > 0)
+            <td>
+                <table
+                        class="tabla-marcador {{ $calificaciones->examen_final ? 'bg-light text-dark' : ($calificaciones->actividades_obligatorias_superadas ? 'bg-success text-white' : 'bg-warning text-dark') }}">
+                    <tr>
+                        <th class="text-left">{{ __('Mandatory activities') }}</th>
+                    </tr>
+                    <tr>
+                        <td class="text-center">{{ $calificaciones->num_actividades_obligatorias > 0 ? $calificaciones->actividades_obligatorias_superadas ? trans_choice('tasks.completed', 2) : ($calificaciones->numero_actividades_completadas+0)."/".($calificaciones->num_actividades_obligatorias+0)  : __('None') }}</td>
+                    </tr>
+                </table>
+            </td>
+        @endif
+        @if($calificaciones->minimo_competencias > 0)
+            <td>
+                <table
+                        class="tabla-marcador {{ $calificaciones->examen_final ? 'bg-light text-dark' : ($calificaciones->competencias_50_porciento ? 'bg-success text-white' : 'bg-warning text-dark') }}">
+                    <tr>
+                        <th class="text-left">{{ __('Skills') }}</th>
+                    </tr>
+                    <tr>
+                        <td class="text-center">{{ $calificaciones->competencias_50_porciento ? trans_choice('tasks.passed', 2) : trans_choice('tasks.not_passed', 2) }}</td>
+                    </tr>
+                </table>
+            </td>
+        @endif
+        @if($calificaciones->num_pruebas_evaluacion > 0)
+            <td>
+                <table
+                        class="tabla-marcador {{ ($curso->examenes_obligatorios || $calificaciones->examen_final) ? ($calificaciones->pruebas_evaluacion || $calificaciones->examen_final_superado) ? 'bg-success text-white' : 'bg-warning text-dark' : 'bg-light text-dark' }}">
+                    <tr>
+                        <th class="text-left">{{ __('Assessment tests') }}</th>
+                    </tr>
+                    <tr>
+                        <td class="text-center">{{ $calificaciones->num_pruebas_evaluacion > 0 ? ($calificaciones->pruebas_evaluacion || $calificaciones->examen_final_superado) ? trans_choice('tasks.passed', 2) : trans_choice('tasks.not_passed', 2) : __('None') }}</td>
+                    </tr>
+                </table>
+            </td>
+        @endif
+        <td>
+            <table
+                    class="tabla-marcador {{ $calificaciones->examen_final ? 'bg-light text-dark' : ($calificaciones->evaluacion_continua_superada ? 'bg-success text-white' : 'bg-warning text-dark') }}">
+                <tr>
+                    <th class="text-left">{{ __('Continuous evaluation') }}</th>
+                </tr>
+                <tr>
+                    <td class="text-center">{{ $calificaciones->evaluacion_continua_superada ? trans_choice('tasks.passed', 1) : trans_choice('tasks.not_passed', 1) }}</td>
+                </tr>
+            </table>
+        </td>
+        <td>
+            <table
+                    class="tabla-marcador {{ ($calificaciones->evaluacion_continua_superada || $calificaciones->examen_final_superado) ? 'bg-success text-white' : ($curso->disponible() ? 'bg-light text-dark' : 'bg-warning text-dark') }}">
+                <tr>
+                    <th class="text-left">{{ __('Calification') }}</th>
+                </tr>
+                <tr>
+                    <td class="text-center">{{ ($calificaciones->evaluacion_continua_superada || $calificaciones->examen_final_superado) ? $calificaciones->nota_final : ($curso->disponible() ? __('Unavailable') : __('Fail')) }}</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
