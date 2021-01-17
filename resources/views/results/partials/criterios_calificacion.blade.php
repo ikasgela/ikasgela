d3b5f58565e3f815f054a59d285b30f88e6026e2
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-01-17 03:18:43 +0100

    Criterios mejorados

--- resources/views/results/partials/criterios_calificacion.blade.php
+++ resources/views/results/partials/criterios_calificacion.blade.php
@@ -6,27 +6,29 @@
             <h5 class="card-title">Criterios de calificación aplicados</h5>
         @endif
         <ul class="card-text mb-1">
-            @if($curso->minimo_entregadas > 0)
-                <li>Se deben haber realizado al menos el {{ formato_decimales($curso->minimo_entregadas) }}&thinsp;% de
-                    las actividades propuestas de cada unidad.
-                    @include('results.partials.criterio_superado', ['criterio' => $calificaciones->actividades_obligatorias_superadas])
-                </li>
-            @endif
-            @if($curso->minimo_competencias > 0)
-                <li>Se debe superar el {{ formato_decimales($curso->minimo_competencias) }}&thinsp;% en cada una de las
-                    competencias de forma individual.
-                    @include('results.partials.criterio_superado', ['criterio' => $calificaciones->competencias_50_porciento])
-                </li>
-            @endif
-            @if($curso->examenes_obligatorios)
-                <li>Se deben haber superado el {{ formato_decimales($curso->minimo_examenes) }}&thinsp;% en las pruebas
-                    teórico-prácticas obligatorias de cada competencia.
-                    @include('results.partials.criterio_superado', ['criterio' => $calificaciones->pruebas_evaluacion])
-                </li>
-            @endif
-            @if($calificaciones->examen_final)
-                <li>Se ha superado el {{ formato_decimales($curso->minimo_examenes) }}&thinsp;% en las pruebas de
-                    evaluación final, recuperando un máximo del 75&thinsp;% de la nota.
+            @if(!$calificaciones->examen_final)
+                @if($curso->minimo_entregadas > 0)
+                    <li>Se deben haber realizado al menos el {{ formato_decimales($curso->minimo_entregadas) }}&thinsp;%
+                        de las actividades propuestas de cada unidad.
+                        @include('results.partials.criterio_superado', ['criterio' => $calificaciones->actividades_obligatorias_superadas])
+                    </li>
+                @endif
+                @if($curso->minimo_competencias > 0)
+                    <li>Se debe superar el {{ formato_decimales($curso->minimo_competencias) }}&thinsp;% en cada una de
+                        las competencias de forma individual.
+                        @include('results.partials.criterio_superado', ['criterio' => $calificaciones->competencias_50_porciento])
+                    </li>
+                @endif
+                @if($curso->examenes_obligatorios)
+                    <li>Se deben haber superado el {{ formato_decimales($curso->minimo_examenes) }}&thinsp;% en las
+                        pruebas teórico-prácticas obligatorias de cada competencia.
+                        @include('results.partials.criterio_superado', ['criterio' => $calificaciones->pruebas_evaluacion])
+                    </li>
+                @endif
+            @else
+                <li>Se debe superar el {{ formato_decimales($curso->minimo_examenes) }}&thinsp;% en las pruebas de
+                    evaluación final, recuperando un máximo del
+                    {{ formato_decimales($curso->maximo_recuperable_examenes_finales) }}&thinsp;% de la nota.
                     @include('results.partials.criterio_superado', ['criterio' => $calificaciones->examen_final_superado])
                 </li>
             @endif
