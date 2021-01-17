b400d2663b45ac60c8e88bdc904ecb1e24e192a5
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-01-17 03:08:31 +0100

    No colorear el examen final si no se ha hecho

--- resources/views/results/pdf/desarrollo_contenidos.blade.php
+++ resources/views/results/pdf/desarrollo_contenidos.blade.php
@@ -17,13 +17,18 @@
                 @endisset
                 @include('unidades.partials.nombre_con_etiquetas', ['pdf' => true])
             </td>
-            <td class="text-center">
-                {{ $calificaciones->resultados_unidades[$unidad->id]->tarea + 0
-             }}/{{ $calificaciones->resultados_unidades[$unidad->id]->actividad + 0 }}
-            </td>
-            <td class="text-center {{ $porcentaje< ($unidad->hasEtiqueta('examen') ? $calificaciones->minimo_examenes : $calificaciones->minimo_competencias) ? 'bg-warning text-dark' : 'bg-success' }}">
-                {{ formato_decimales($porcentaje) }}&thinsp;%
-            </td>
+            @if($unidad->hasEtiquetas(['examen','final']) && !$calificaciones->examen_final)
+                <td class="text-center">-</td>
+                <td class="text-center">-</td>
+            @else
+                <td class="text-center">
+                    {{ $calificaciones->resultados_unidades[$unidad->id]->tarea + 0
+                 }}/{{ $calificaciones->resultados_unidades[$unidad->id]->actividad + 0 }}
+                </td>
+                <td class="text-center {{ $porcentaje< ($unidad->hasEtiqueta('examen') ? $calificaciones->minimo_examenes : $calificaciones->minimo_competencias) ? 'bg-warning text-dark' : 'bg-success' }}">
+                    {{ formato_decimales($porcentaje) }}&thinsp;%
+                </td>
+            @endif
         </tr>
     @endforeach
 </table>
