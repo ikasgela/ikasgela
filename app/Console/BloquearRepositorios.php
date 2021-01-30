8194b9c4d37d44daa8f7ab31080e0d9ab551f2cc
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-01-30 19:10:49 +0100

    Bloquear solo los repositorios que tengan fork

--- app/Console/BloquearRepositorios.php
+++ app/Console/BloquearRepositorios.php
@@ -3,7 +3,6 @@
 namespace App\Console;
 
 use App\Actividad;
-use Carbon\Carbon;
 use Log;
 
 class BloquearRepositorios
@@ -11,7 +10,7 @@ class BloquearRepositorios
     public function __invoke()
     {
         $actividades = Actividad::where('plantilla', false)
-            ->where('fecha_limite', '<=', Carbon::now())->get();
+            ->where('is_expired', true)->get();
 
         $total = 0;
         $archivados = 0;
@@ -19,7 +18,7 @@ class BloquearRepositorios
         foreach ($actividades as $actividad) {
             foreach ($actividad->intellij_projects as $intellij_project) {
 
-                if (!$intellij_project->isArchivado()) {
+                if (!$intellij_project->isArchivado() && $intellij_project->isForked()) {
                     $intellij_project->archive();
                     $archivados += 1;
                 }
