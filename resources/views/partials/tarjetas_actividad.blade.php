4abad77be2ab9eaa003a441a978202c45f4a430d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-04-25 13:06:00 +0200

    Propiedad en vez de métodos

--- resources/views/partials/tarjetas_actividad.blade.php
+++ resources/views/partials/tarjetas_actividad.blade.php
@@ -4,34 +4,34 @@
             @include('teams.partials.tarjeta', ['team' => $team])
         </div>
     @endforeach
-@foreach($actividad->markdown_texts()->get() as $markdown_text)
-    <div class="col-md-6">
-        @include('markdown_texts.tarjeta', ['texto' => $markdown_text->markdown()])
-    </div>
-@endforeach
-@foreach($actividad->youtube_videos()->get() as $youtube_video)
-    <div class="col-md-6">
-        @include('youtube_videos.tarjeta')
-    </div>
-@endforeach
-@foreach($actividad->file_resources()->get() as $file_resource)
-    <div class="col-md-6">
-        @include('file_resources.tarjeta')
-    </div>
-@endforeach
-@foreach($actividad->cuestionarios()->get() as $cuestionario)
-    <div class="col-md-6">
-        @include('cuestionarios.tarjeta')
-    </div>
-@endforeach
-@foreach($actividad->file_uploads()->get() as $file_upload)
-    <div class="col-md-6">
-        @include('file_uploads.tarjeta')
-    </div>
-@endforeach
-@foreach($actividad->intellij_projects()->get() as $intellij_project)
-    <div class="col-md-6">
-        @include('intellij_projects.tarjeta', ['repositorio' => $intellij_project->repository()])
-    </div>
-@endforeach
+    @foreach($actividad->markdown_texts as $markdown_text)
+        <div class="col-md-6">
+            @include('markdown_texts.tarjeta', ['texto' => $markdown_text->markdown()])
+        </div>
+    @endforeach
+    @foreach($actividad->youtube_videos as $youtube_video)
+        <div class="col-md-6">
+            @include('youtube_videos.tarjeta')
+        </div>
+    @endforeach
+    @foreach($actividad->file_resources as $file_resource)
+        <div class="col-md-6">
+            @include('file_resources.tarjeta')
+        </div>
+    @endforeach
+    @foreach($actividad->cuestionarios as $cuestionario)
+        <div class="col-md-6">
+            @include('cuestionarios.tarjeta')
+        </div>
+    @endforeach
+    @foreach($actividad->file_uploads as $file_upload)
+        <div class="col-md-6">
+            @include('file_uploads.tarjeta')
+        </div>
+    @endforeach
+    @foreach($actividad->intellij_projects as $intellij_project)
+        <div class="col-md-6">
+            @include('intellij_projects.tarjeta', ['repositorio' => $intellij_project->repository()])
+        </div>
+    @endforeach
 </div>
