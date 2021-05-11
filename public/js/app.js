69680104aab5473c6bc00f659e1cd6bb53c64e61
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-05-11 16:27:46 +0200

    Actualización de dependencias

--- public/js/app.js
+++ public/js/app.js
@@ -61450,7 +61450,7 @@ function normalizeComponent (
 /******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
 /******/ 				}
 /******/ 			}
-/******/ 			if(runtime) runtime(__webpack_require__);
+/******/ 			if(runtime) var result = runtime(__webpack_require__);
 /******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
 /******/ 			for(;i < chunkIds.length; i++) {
 /******/ 				chunkId = chunkIds[i];
@@ -61459,7 +61459,7 @@ function normalizeComponent (
 /******/ 				}
 /******/ 				installedChunks[chunkIds[i]] = 0;
 /******/ 			}
-/******/ 			__webpack_require__.O();
+/******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
 /******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
