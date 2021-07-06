e209d1727f5768f6b4ac454961d84cf6e6f40e65
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-07-06 11:48:45 +0200

    Revisar el factory

--- database/factories/QualificationFactory.php
+++ database/factories/QualificationFactory.php
@@ -4,14 +4,14 @@ namespace Database\Factories;
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
-use App\Organization;
+use App\Curso;
 use App\Qualification;
 use Faker\Generator as Faker;
 
 $factory->define(Qualification::class, function (Faker $faker) {
 
     return [
-        'organization_id' => factory(Organization::class),
+        'curso_id' => factory(Curso::class),
         'name' => $faker->words(3, true),
         'description' => $faker->sentence(),
     ];
