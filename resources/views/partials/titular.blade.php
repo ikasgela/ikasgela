e55c45bcc7d7ecbf51cb696579627d398929a0c0
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-07-15 16:54:19 +0200

    Mostrar por defecto el curso actual

--- resources/views/partials/titular.blade.php
+++ resources/views/partials/titular.blade.php
@@ -2,5 +2,7 @@
     <h1>{{ $titular }}</h1>
     @if(isset($subtitulo))
         <h2 class="text-muted font-xl">{{ $subtitulo }}</h2>
+    @else
+        <h2 class="text-muted font-xl">{{ Auth::user()->curso_actual()?->full_name }}</h2>
     @endif
 </div>
