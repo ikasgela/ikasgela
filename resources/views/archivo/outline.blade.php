765dd4fb4917a6f897663c488279ff6e1785d1f6
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-07-19 15:04:59 +0200

    No dar error si no hay curso asignado

--- resources/views/archivo/outline.blade.php
+++ resources/views/archivo/outline.blade.php
@@ -11,10 +11,10 @@
 
     <div class="c-callout c-callout-bordered p-3">
         <h5><strong>Fecha de inicio:</strong>
-            {{ $curso->fecha_inicio ? $curso->fecha_inicio->format('d/m/Y H:i') : __('Undefined') }}
+            {{ $curso?->fecha_inicio ? $curso->fecha_inicio->format('d/m/Y H:i') : __('Undefined') }}
         </h5>
         <h5 class="mb-0"><strong>Fecha de final:</strong>
-            {{ $curso->fecha_fin ? $curso->fecha_fin->format('d/m/Y H:i') : __('Undefined') }}</h5>
+            {{ $curso?->fecha_fin ? $curso->fecha_fin->format('d/m/Y H:i') : __('Undefined') }}</h5>
     </div>
 
     @if(count($unidades) > 0)
