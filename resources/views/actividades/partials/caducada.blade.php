76d92c4de302f806b3e7f74c3332e81d06bb67ec
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-07-21 10:00:03 +0200

    Mostrar las plantillas caducadas en gris

--- resources/views/actividades/partials/caducada.blade.php
+++ resources/views/actividades/partials/caducada.blade.php
@@ -1,3 +1,7 @@
-{!! !$actividad->hasEtiqueta('examen')
-        && ($actividad->is_finished || $actividad->is_expired)
-        ? '<i class="fas fa-exclamation-triangle text-warning" title="'.trans_choice('tasks.expired', 1).'"></i>' : '' !!}
+@if($actividad->is_finished || $actividad->is_expired)
+    @if($actividad->plantilla)
+        <i class="fas fa-exclamation-triangle text-secondary" title="{{ trans_choice('tasks.expired', 1) }}"></i>
+    @else
+        <i class="fas fa-exclamation-triangle text-warning" title="{{ trans_choice('tasks.expired', 1) }}"></i>
+    @endif
+@endif
