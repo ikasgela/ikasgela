7d70c30faf1746bf9a8dbcd6f9adc6edb07dc0b9
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-09-22 00:27:42 +0200

    Barra de navegación con color de fondo
    https://css-tricks.com/safari-15-new-ui-theme-colors-and-a-css-tricks-cameo/

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="theme-color" content="{{ config('app.debug') ? '#ffc107' : '#3490dc' }}">
     <title>
         {{ config('app.name', 'Laravel') }}
         {{ subdominio() != 'ikasgela' ? ' | '. subdominio() :  '' }}
