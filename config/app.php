506336477cc36155cd33dc527adefedd12e73a27
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-10-04 23:55:57 +0200

    Restos del paquete de GitLab

--- config/app.php
+++ config/app.php
@@ -214,8 +214,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'GitLab' => GrahamCampbell\GitLab\Facades\GitLab::class,
-
         'Embed' => Cohensive\Embed\Facades\Embed::class,
     ],
 
