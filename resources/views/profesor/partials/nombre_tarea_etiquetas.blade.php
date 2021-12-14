fc0ef57a8d4b2c2a20200e088dfc8ea14963f931
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-12-14 09:36:13 +0100

    Espaciado corregido

--- resources/views/profesor/partials/nombre_tarea_etiquetas.blade.php
+++ resources/views/profesor/partials/nombre_tarea_etiquetas.blade.php
@@ -1,4 +1,4 @@
 <span class="mr-2">{{ $actividad->unidad->slug.'/'.$actividad->slug }}</span>
 @foreach($actividad->etiquetas() as $etiqueta)
-    {!! '<a class="badge badge-secondary ml-2" href="'.route(explode('.',Route::currentRouteName())[0].'.tareas.filtro',['user'=>$user->id, 'tag'=>$etiqueta]).'">'.$etiqueta.'</a>' !!}
+    {!! '<a class="badge badge-secondary mr-2" href="'.route(explode('.',Route::currentRouteName())[0].'.tareas.filtro',['user'=>$user->id, 'tag'=>$etiqueta]).'">'.$etiqueta.'</a>' !!}
 @endforeach
