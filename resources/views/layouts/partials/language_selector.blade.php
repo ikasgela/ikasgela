1d77a7b32bf15eee006dd8807c56742b154fe255
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-12-18 12:15:23 +0100

    Mostrar el título del enlace en el selector

--- resources/views/layouts/partials/language_selector.blade.php
+++ resources/views/layouts/partials/language_selector.blade.php
@@ -2,7 +2,7 @@
     @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
         @if(LaravelLocalization::getCurrentLocale() != $localeCode)
             <li class="pl-2">
-                <a rel="alternate" hreflang="{{ $localeCode }}"
+                <a rel="alternate" hreflang="{{ $localeCode }}" title="{{ $properties['native'] }}"
                    href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
                     <span class="flag-icon flag-icon-{{ $localeCode == 'en' ? 'gb' : $localeCode }}"></span>
                 </a>
