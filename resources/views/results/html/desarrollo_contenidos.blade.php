c6a1ca51c469abd1bd2d1f09b5258bff2b1506c0
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-12-21 12:36:47 +0100

    Mejorar la visualización de los porcentajes pequeños

--- resources/views/results/html/desarrollo_contenidos.blade.php
+++ resources/views/results/html/desarrollo_contenidos.blade.php
@@ -11,22 +11,30 @@
             </h5>
             <p class="ml-5">{{ $unidad->descripcion }}</p>
             <div class="ml-5 progress" style="height: 24px;">
-                @php($porcentaje = $calificaciones->resultados_unidades[$unidad->id]->actividad > 0 ? round($calificaciones->resultados_unidades[$unidad->id]->tarea/$calificaciones->resultados_unidades[$unidad->id]->actividad*100) : 0)
+                @php($hay_calificacion = $calificaciones->resultados_unidades[$unidad->id]->actividad > 0)
+                @php($porcentaje = $hay_calificacion ? round($calificaciones->resultados_unidades[$unidad->id]->tarea/$calificaciones->resultados_unidades[$unidad->id]->actividad*100) : 0)
                 <div
                     class="progress-bar {{ $porcentaje< ($unidad->hasEtiqueta('examen') ? $calificaciones->minimo_examenes : $calificaciones->minimo_competencias) ? 'bg-warning text-dark' : 'bg-success' }}"
                     role="progressbar"
                     style="width: {{ $porcentaje }}%;"
                     aria-valuenow="{{ $porcentaje }}"
                     aria-valuemin="0"
-                    aria-valuemax="100">@if($porcentaje>=15){{ $porcentaje }}&thinsp;%@endif
+                    aria-valuemax="100">@if($porcentaje>=20){{ $porcentaje }}&thinsp;%@endif
                 </div>
-                @if($porcentaje<15)
-                    <div class="progress-bar bg-gray-200 text-dark pl-1">{{ $porcentaje }}&thinsp;%</div>
+                @if($hay_calificacion && $porcentaje<20)
+                    <div
+                        class="progress-bar {{ $porcentaje > 0 ? 'bg-gray-200' : 'bg-warning w-100 text-left' }} text-dark pl-2">
+                        {{ $porcentaje }}&thinsp;%
+                    </div>
                 @endif
             </div>
             <div class="text-muted small text-right">
-                {{ $calificaciones->resultados_unidades[$unidad->id]->tarea + 0
-                }}/{{ $calificaciones->resultados_unidades[$unidad->id]->actividad + 0 }}
+                @if($hay_calificacion)
+                    {{ $calificaciones->resultados_unidades[$unidad->id]->tarea + 0
+                    }}/{{ $calificaciones->resultados_unidades[$unidad->id]->actividad + 0 }}
+                @else
+                    {{ __('No results yet') }}
+                @endif
             </div>
             @if(!$loop->last)
                 <hr>
