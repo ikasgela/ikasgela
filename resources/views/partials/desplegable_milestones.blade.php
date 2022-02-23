a03acdc9612b74a9d211d2821f1a6c88ae440ea9
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-02-23 17:04:35 +0100

    Mensaje mejorado

--- resources/views/partials/desplegable_milestones.blade.php
+++ resources/views/partials/desplegable_milestones.blade.php
@@ -2,7 +2,7 @@
     {!! Form::label('milestone_id', __('Milestone'), ['class' => 'col-form-label']) !!}
     <div class="flex-fill mx-3">
         <select class="custom-select" id="milestone_id" name="milestone_id">
-            <option value="-1">{{ __('--- None ---') }}</option>
+            <option value="-1">{{ __('--- None (whole course) ---') }}</option>
             @foreach($milestones as $milestone)
                 <option
                     value="{{ $milestone->id }}" {{ session('filtrar_milestone_actual') == $milestone->id ? 'selected' : '' }}>
