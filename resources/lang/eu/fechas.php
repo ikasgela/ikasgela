4bd990cb1f19fc0c0c0efe2fb1c60a77e1c5b68d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-09-04 10:56:45 +0200

    Más traducciones

---
+++ resources/lang/eu/fechas.php
@@ -0,0 +1,8 @@
+<?php
+
+// resources/lang/es/fechas.php
+
+return [
+    'remaining' => '{0}Falta menos de un día|{1}Falta un día|[2,*]Faltan :days días',
+    'expired' => 'Fecha límite superada',
+];
---
+++ resources/lang/eu/tutorial.php
@@ -0,0 +1,27 @@
+<?php
+
+// resources/lang/es/tutorial.php
+
+return [
+    'ocultable' => 'Tutoriala zure erabiltzailearen profilean desaktibatu dezakezu.',
+    'para_comenzar' => 'Para comenzar la actividad, acéptala. A partir de ese momento tendrás acceso a sus recursos.',
+    'completa_envia' => 'Completa la actividad y, cuando esté lista, envíala para revisar.',
+    'pendiente_revisar' => 'La actividad está pendiente de revisar, recibirás un email cuando se complete la revisión.',
+    'revisada' => 'La actividad está revisada y tienes disponible el feedback.<br> Si se ha dado por buena, podrás darla por terminada y trasladarla al <a href=":url">archivo</a>. Si no, tendrás que mejorarla y volver a enviarla.',
+    'terminada' => 'La actividad está terminada y puedes archivarla para que desaparezca del escritorio. Podrás verla en el <a href=":url">archivo</a>.',
+    'archivada' => 'Esta es una actividad archivada.',
+    'disponibles' => 'Eskuragarri dituzun aktibitateak hemen agertuko dira.',
+    'grafico' => 'El gráfico muestra el total de actividades enviadas desde la fecha de inicio del curso.',
+    'vista_previa' => 'Vista previa de la actividad.',
+    'matricula' => 'Aquí puedes matricularte en otros cursos disponibles y cambiar de un curso a otro.',
+    'archivo' => 'Aquí aparecerán las tareas una vez que las completes.',
+    'todas_actividades' => 'Todas las actividades del curso.',
+    'mensajes' => 'Aquí puedes iniciar conversaciones para ayudarte a resolver tus dudas sobre las actividades.',
+    'valorar_actividad' => 'Aquí puedes valorar la actividad y dar el feedback oportuno.',
+    'resultados' => 'Aquí aparecerán los resultados de las competencias asociadas al curso.',
+    'cambiar_curso' => 'Si estás matriculado en más de un curso, aquí puedes cambiar de uno a otro.',
+    'resultados_tutor' => 'Aquí aparecerá la tabla con los resultados de todos los alumnos del grupo. Se puede ordenar alfabéticamente o por número de actividades completadas.',
+    'notificaciones' => 'Activar o desactivar las notificaciones por email.',
+    'perfil' => 'Actualiza tu perfil de usuario.',
+    'password' => 'Actualiza tu contraseña.',
+];
