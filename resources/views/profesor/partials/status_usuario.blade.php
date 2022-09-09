092998e545c57c050a6d5d3f96041b920d258dbd
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-09-09 11:37:55 +0200

    Destacar los usuarios bloqueados y sin verificar

--- resources/views/profesor/partials/status_usuario.blade.php
+++ resources/views/profesor/partials/status_usuario.blade.php
@@ -1,5 +1,5 @@
-{!! $user->isBlocked() ? '<span class="badge badge-secondary ml-2">'.__('Blocked').'</span>' : '' !!}
-{!! !$user->isVerified() ? '<span class="badge badge-secondary ml-2">'.__('Unverified').'</span>' : '' !!}
+{!! $user->isBlocked() ? '<span class="badge badge-danger ml-2">'.__('Blocked').'</span>' : '' !!}
+{!! !$user->isVerified() ? '<span class="badge badge-warning ml-2">'.__('Unverified').'</span>' : '' !!}
 @foreach($user->etiquetas() as $etiqueta)
     {!! '<span class="badge badge-secondary ml-2">'.$etiqueta.'</span>' !!}
 @endforeach
