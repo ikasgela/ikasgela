59798ba1ed679b038f4d7ae4496ce88b1540e791
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-09-10 18:11:43 +0200

    Solo añadir la tarea de bienvenida si no hay ninguna

--- app/Traits/TareaBienvenida.php
+++ app/Traits/TareaBienvenida.php
@@ -10,17 +10,19 @@ trait TareaBienvenida
 {
     public function asignarTareaBienvenida(Curso $curso, User $user): void
     {
-        $actividad = Actividad::whereHas('unidad.curso', function ($query) use ($curso) {
-            $query->where('id', $curso->id);
-        })->where('slug', 'tarea-de-bienvenida')
-            ->where('plantilla', true)
-            ->first();
+        if ($user->actividades()->count() == 0) {
+            $actividad = Actividad::whereHas('unidad.curso', function ($query) use ($curso) {
+                $query->where('id', $curso->id);
+            })->where('slug', 'tarea-de-bienvenida')
+                ->where('plantilla', true)
+                ->first();
 
-        if (isset($actividad)) {
-            $clon = $actividad->duplicate();
-            $clon->plantilla_id = $actividad->id;
-            $clon->save();
-            $user->actividades()->attach($clon, ['puntuacion' => $actividad->puntuacion]);
+            if (isset($actividad)) {
+                $clon = $actividad->duplicate();
+                $clon->plantilla_id = $actividad->id;
+                $clon->save();
+                $user->actividades()->attach($clon, ['puntuacion' => $actividad->puntuacion]);
+            }
         }
     }
 }
