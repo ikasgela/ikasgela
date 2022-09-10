d9c71ae40b5a2d7f841923e5c3d8752fb9a9dd2b
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-09-10 13:03:03 +0200

    Refactor de la lista de etiquetas

--- resources/views/actividades/partials/encabezado_con_etiquetas.blade.php
+++ resources/views/actividades/partials/encabezado_con_etiquetas.blade.php
@@ -1,8 +1,6 @@
 <div class="d-inline-flex align-items-center">
     <h2>{{ $actividad->nombre }}</h2>
     <div class="mb-2 ml-3">
-        @foreach($actividad->etiquetas() as $etiqueta)
-            {!! '<span class="badge badge-secondary">'.$etiqueta.'</span>' !!}
-        @endforeach
+        @include('partials.etiquetas', ['etiquetas' => $actividad->etiquetas()])
     </div>
 </div>
---
+++ resources/views/partials/etiquetas.blade.php
@@ -0,0 +1,3 @@
+@foreach($etiquetas as $etiqueta)
+    {!! '<span class="badge badge-secondary">' . $etiqueta . '</span>' !!}
+@endforeach
