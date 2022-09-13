eab47f3b47e514f4b696c874da3592631d386efb
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-09-13 18:45:16 +0200

    Plugin para seleccionar más de un checkbox a la vez

--- resources/js/app.js
+++ resources/js/app.js
@@ -131,4 +131,29 @@ $(document).ready(function ($) {
             sidebar_open: is_sidebar_open,
         });
     });
+
+    // https://github.com/iTeeLion/jquery.checkbox-shift-selector.js
+    var chkboxShiftLastChecked = [];
+
+    $('[data-chkbox-shiftsel]').click(function (e) {
+        var chkboxType = $(this).data('chkbox-shiftsel');
+        if (chkboxType === '') {
+            chkboxType = 'default';
+        }
+        var $chkboxes = $('[data-chkbox-shiftsel="' + chkboxType + '"]');
+
+        if (!chkboxShiftLastChecked[chkboxType]) {
+            chkboxShiftLastChecked[chkboxType] = this;
+            return;
+        }
+
+        if (e.shiftKey) {
+            var start = $chkboxes.index(this);
+            var end = $chkboxes.index(chkboxShiftLastChecked[chkboxType]);
+
+            $chkboxes.slice(Math.min(start, end), Math.max(start, end) + 1).prop('checked', chkboxShiftLastChecked[chkboxType].checked);
+        }
+
+        chkboxShiftLastChecked[chkboxType] = this;
+    });
 });
