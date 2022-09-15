42650a323cb50e8f4af12e185f7755b6cbeba6b3
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-09-15 10:34:46 +0200

    Agrupar los elementos del menú tutor

--- resources/views/menus/tutor.blade.php
+++ resources/views/menus/tutor.blade.php
@@ -5,22 +5,29 @@
     </a>
 </li>
 <li class="c-sidebar-nav-item">
-    <a class="c-sidebar-nav-link" href="{{ route('results.index') }}">
-        <i class="c-sidebar-nav-icon fas fa-graduation-cap"></i> {{ __('Results') }}
-    </a>
-</li>
-<li class="c-sidebar-nav-item">
     <a class="c-sidebar-nav-link" href="{{ route('tutor.tareas_enviadas') }}">
         <i class="c-sidebar-nav-icon fas fa-chart-bar"></i> {{ __('Activities per day') }}
     </a>
 </li>
-<li class="c-sidebar-nav-item">
-    <a class="c-sidebar-nav-link" href="{{ route('archivo.outline') }}">
-        <i class="c-sidebar-nav-icon fas fa-list"></i> {{ __('Course progress') }}
+<li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
+    <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
+        <i class="c-sidebar-nav-icon fas fa-user-check"></i> {{ __('Individual reports') }}
     </a>
-</li>
-<li class="c-sidebar-nav-item">
-    <a class="c-sidebar-nav-link" href="{{ route('archivo.index') }}">
-        <i class="c-sidebar-nav-icon fas fa-archive"></i> {{ __('Archived') }}
-    </a>
+    <ul class="c-sidebar-nav-dropdown-items">
+        <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link bg-dark" href="{{ route('results.index') }}">
+                <i class="c-sidebar-nav-icon fas fa-graduation-cap"></i> {{ __('Results') }}
+            </a>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link bg-dark" href="{{ route('archivo.outline') }}">
+                <i class="c-sidebar-nav-icon fas fa-list"></i> {{ __('Course progress') }}
+            </a>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link bg-dark" href="{{ route('archivo.index') }}">
+                <i class="c-sidebar-nav-icon fas fa-archive"></i> {{ __('Archived') }}
+            </a>
+        </li>
+    </ul>
 </li>
