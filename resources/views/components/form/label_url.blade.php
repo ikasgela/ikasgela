ed5aa0df37bc568676f4145b672353ec0f02896a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-09-27 08:39:38 +0200

    Abrir en ventana nueva

--- resources/views/components/form/label_url.blade.php
+++ resources/views/components/form/label_url.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group row">
     {!! Form::label($name, $label, ['class' => 'col-sm-2 col-form-label']) !!}
     <div class="col-sm-10">
-        <a class="btn btn-link" href="{{ $value }}">{{ $value }}</a>
+        <a class="btn btn-link" target="_blank" href="{{ $value }}">{{ $value }}</a>
     </div>
 </div>
