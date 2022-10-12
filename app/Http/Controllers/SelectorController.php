da2994c3f5c959885e755a22b0573cba5c3da361
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-10-12 11:33:09 +0200

    Controlador

--- app/Http/Controllers/SelectorController.php
+++ app/Http/Controllers/SelectorController.php
@@ -2,85 +2,119 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreSelectorRequest;
-use App\Http\Requests\UpdateSelectorRequest;
+use App\Models\Actividad;
+use App\Models\Curso;
 use App\Models\Selector;
+use App\Traits\FiltroCurso;
+use App\Traits\PaginarUltima;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class SelectorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    use PaginarUltima;
+    use FiltroCurso;
+
+    public function __construct()
     {
-        //
+        $this->middleware('auth');
+        $this->middleware('role:profesor|admin');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function index(Request $request)
+    {
+        $cursos = Curso::orderBy('nombre')->get();
+
+        $selectors = $this->filtrar_por_curso($request, Selector::class)->get();
+
+        return view('selectors.index', compact(['selectors', 'cursos']));
+    }
+
     public function create()
     {
-        //
+        return view('selectors.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreSelectorRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreSelectorRequest $request)
+    public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'titulo' => 'required',
+        ]);
+
+        $selector = Selector::create([
+            'titulo' => $request->input('titulo'),
+            'descripcion' => $request->input('descripcion'),
+            'curso_id' => $request->has('curso_id') ? request('curso_id') : Auth::user()->curso_actual()?->id,
+        ]);
+
+        olvidar();
+
+        return redirect(route('selectors.edit', ['selector' => $selector]));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Selector  $selector
-     * @return \Illuminate\Http\Response
-     */
     public function show(Selector $selector)
     {
-        //
+        return view('selectors.show', compact(['selector']));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Selector  $selector
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Selector $selector)
     {
-        //
+        $rule_groups = $selector->rule_groups()->get();
+        $ids = $rule_groups->pluck('id')->toArray();
+
+        return view('selectors.edit', compact(['selector', 'rule_groups', 'ids']));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateSelectorRequest  $request
-     * @param  \App\Models\Selector  $selector
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateSelectorRequest $request, Selector $selector)
+    public function update(Request $request, Selector $selector)
     {
-        //
+        $this->validate($request, [
+            'titulo' => 'required',
+        ]);
+
+        $selector->update([
+            'titulo' => $request->input('titulo'),
+            'descripcion' => $request->input('descripcion'),
+        ]);
+
+        return retornar();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Selector  $selector
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Selector $selector)
     {
-        //
+        $selector->delete();
+
+        return back();
+    }
+
+    public function actividad(Actividad $actividad)
+    {
+        $selectors = $actividad->selectors()->get();
+
+        $subset = $selectors->pluck('id')->unique()->flatten()->toArray();
+        $curso_actual = Auth::user()->curso_actual()->id;
+        $disponibles = $this->paginate_ultima(Selector::where('curso_id', $curso_actual)->whereNotIn('id', $subset));
+
+        return view('selectors.actividad', compact(['selectors', 'disponibles', 'actividad']));
+    }
+
+    public function asociar(Actividad $actividad, Request $request)
+    {
+        $this->validate($request, [
+            'seleccionadas' => 'required',
+        ]);
+
+        foreach (request('seleccionadas') as $recurso_id) {
+            $recurso = Selector::find($recurso_id);
+            $actividad->selectors()->attach($recurso, ['orden' => Str::orderedUuid()]);
+        }
+
+        return redirect(route('selectors.actividad', ['actividad' => $actividad->id]));
+    }
+
+    public function desasociar(Actividad $actividad, Selector $selector)
+    {
+        $actividad->selectors()->detach($selector);
+        return redirect(route('selectors.actividad', ['actividad' => $actividad->id]));
     }
 }
