6ed232eab0bd65689e32f11bc01c0373913e3b9e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-10-19 14:44:26 +0200

    Quitar la localización manual del test de notificación

--- app/Mail/NotificationTest.php
+++ app/Mail/NotificationTest.php
@@ -5,33 +5,13 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\App;
 
 class NotificationTest extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public $locale;
-
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct($locale = 'en')
-    {
-        $this->locale = $locale;
-    }
-
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
     public function build()
     {
-        App::setLocale($this->locale);
-
         return $this
             ->subject(__('Notification test'))
             ->markdown('emails.notification_test');
