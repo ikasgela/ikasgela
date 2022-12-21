a49c3a26608b6739c6ff940374aad538baafc8be
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-12-22 00:31:53 +0100

    Asignar la tarea de bienvenida del curso

--- app/Traits/TareaBienvenida.php
+++ app/Traits/TareaBienvenida.php
@@ -11,11 +11,15 @@ trait TareaBienvenida
     public function asignarTareaBienvenida(Curso $curso, User $user): void
     {
         if ($user->actividades()->count() == 0) {
-            $actividad = Actividad::whereHas('unidad.curso', function ($query) use ($curso) {
-                $query->where('id', $curso->id);
-            })->where('slug', 'tarea-de-bienvenida')
-                ->where('plantilla', true)
-                ->first();
+            if (isset($curso->tarea_bienvenida_id)) {
+                $actividad = Actividad::find($curso->tarea_bienvenida_id);
+            } else {
+                $actividad = Actividad::whereHas('unidad.curso', function ($query) use ($curso) {
+                    $query->where('id', $curso->id);
+                })->where('slug', 'tarea-de-bienvenida')
+                    ->where('plantilla', true)
+                    ->first();
+            }
 
             if (isset($actividad)) {
                 $clon = $actividad->duplicate();
