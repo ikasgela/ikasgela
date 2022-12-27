66c8236c37a7d82db3b7d974d8e207e32b868007
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-12-27 12:05:29 +0100

    Desactivado el ide-helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -50,6 +50,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if (!$this->app->environment('production')) {
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
             $this->app->register(\BeyondCode\DumpServer\DumpServerServiceProvider::class);
             $this->app->register(\Laravel\Dusk\DuskServiceProvider::class);
         }
