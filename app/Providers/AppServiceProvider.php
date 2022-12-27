e1c6532a2fdbd6d43c6356163338aac8b8684234
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-12-27 11:04:09 +0100

    No cargar automáticamente el paquete en producción

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -48,6 +48,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (!$this->app->environment('production')) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
     }
 }
