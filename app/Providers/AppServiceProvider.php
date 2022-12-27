5661e8ea9534ffc6b75ae0f8ad57b1453ede61ee
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-12-27 12:24:46 +0100

    Paquete eliminado

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -51,7 +51,6 @@ class AppServiceProvider extends ServiceProvider
         if (!$this->app->environment('production')) {
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-            $this->app->register(\BeyondCode\DumpServer\DumpServerServiceProvider::class);
             $this->app->register(\Laravel\Dusk\DuskServiceProvider::class);
             $this->app->register(\Spatie\LaravelIgnition\IgnitionServiceProvider::class);
             $this->app->register(\Laravel\Tinker\TinkerServiceProvider::class);
