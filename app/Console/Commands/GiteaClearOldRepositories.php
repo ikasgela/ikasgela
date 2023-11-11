4dc0adbfbfc8cfb35c1d4a1ff3730d75b896de98
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2023-11-11 21:54:47 +0100

    Comando para limpiar repositorios viejos

---
+++ app/Console/Commands/GiteaClearOldRepositories.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Actividad;
+use Cache;
+use DB;
+use Ikasgela\Gitea\GiteaClient;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class GiteaClearOldRepositories extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gitea:clear-old-repositories';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Limpiar repositorios antiguos';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if (config('app.env') == 'production') {
+            $this->alert('App en producción');
+            if ($this->confirm('¿Continuar?')) {
+
+                $this->info('Inicio: ' . now());
+
+                $result = DB::table('actividad_intellij_project')->where('fork_status', '>', 0)->get();
+
+                $total = 0;
+
+                foreach ($result->values() as $proyecto) {
+
+                    $repo_id = null;
+                    $user_id = null;
+                    try {
+                        $repo_id = GiteaClient::repo($proyecto->fork)['id'];
+                    } catch (\Exception $e) {
+                        $username = Str::before($proyecto->fork, '/');
+                        try {
+                            $user_id = GiteaClient::uid($username);
+                        } catch (\Exception $e) {
+                        }
+                    }
+
+                    if ($repo_id == null && $user_id == null) {
+                        $total++;
+                        $this->info("Repositorio huerfano: " . $proyecto->fork);
+                        DB::table('actividad_intellij_project')->where('id', '=', $proyecto->id)->delete();
+                    }
+                }
+
+                $this->line('');
+                $this->warn('Total: ' . $total);
+
+                $this->info('Fin: ' . now());
+            }
+        }
+    }
+}
