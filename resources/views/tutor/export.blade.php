3856ff07e04d49f95d3889a435dd4127a589a587
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2023-11-20 17:58:21 +0100

    Informe mejorado

--- resources/views/tutor/export.blade.php
+++ resources/views/tutor/export.blade.php
@@ -1,10 +1,16 @@
 <div class="d-flex flex-row flex-wrap justify-content-between align-items-baseline mb-3">
-    <h1>{{ __('Group report') }}</h1>
-    <h2 class="text-muted font-xl">{{ !is_null($curso) ? $curso->category->period->organization->name.' » '.$curso->category->period->name.' » '.$curso->nombre : '' }}</h2>
+    <h3>{{ __('Group report') }}</h3>
+    @isset($curso)
+        <h5>{{ $curso->pretty_name }}</h5>
+    @endisset
+    @isset($milestone)
+        <h5>{{ $milestone->name . ' (' . $milestone->date->isoFormat('L LT') . ')' }}</h5>
+    @endisset
+    <p></p>
 </div>
 
 @include('tutor.partials.tabla_usuarios', ['exportar' => true])
 
 <div>
-    <p class="text-center text-muted font-xs">{{ now()->isoFormat('L LTS') }}</p>
+    <p>{{ now()->isoFormat('L LTS') }}</p>
 </div>
