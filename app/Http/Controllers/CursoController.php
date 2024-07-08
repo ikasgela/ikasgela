e87636a471b10b7b7b524ac8f5b8dbc1c0bebd36
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-07-08 15:28:31 +0200

    Borrar el curso en un job de fondo

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreFile;
+use App\Jobs\BorrarCurso;
 use App\Jobs\ImportCurso;
 use App\Models\Category;
 use App\Models\Curso;
@@ -13,7 +14,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File as SystemFile;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Log;
@@ -140,89 +140,7 @@ class CursoController extends Controller
 
     public function destroy(Curso $curso)
     {
-        foreach ($curso->actividades()->get() as $actividad) {
-            DB::table('tareas')
-                ->where('actividad_id', '=', $actividad->id)
-                ->delete();
-
-            DB::table('actividad_team')
-                ->where('actividad_id', '=', $actividad->id)
-                ->delete();
-
-            $actividad->feedbacks()->delete();
-        }
-
-        DB::table('curso_user')
-            ->where('curso_id', '=', $curso->id)
-            ->delete();
-
-        DB::table('settings')
-            ->where('key', '=', 'curso_actual')
-            ->where('value', '=', $curso->id)
-            ->delete();
-
-        /*
-        foreach ($curso->intellij_projects as $intellij_project) {
-            $repo = $intellij_project->repository();
-            GiteaClient::borrar_repo($repo['id']);
-        }
-        */
-
-        $curso->intellij_projects()->delete();
-        GiteaClient::borrar_organizacion($curso->slug);
-        $curso->markdown_texts()->delete();
-        $curso->youtube_videos()->delete();
-        $curso->cuestionarios()->delete();
-
-        foreach ($curso->file_resources_files as $file) {
-            $file->delete();
-        }
-        $curso->file_resources()->delete();
-
-        foreach ($curso->file_uploads_files as $file) {
-            $file->delete();
-        }
-        $curso->file_uploads()->delete();
-
-        $curso->link_collections_links()->delete();
-        $curso->link_collections()->delete();
-
-        Schema::disableForeignKeyConstraints();
-        $curso->actividades()->forceDelete();
-        Schema::enableForeignKeyConstraints();
-
-        $curso->unidades()->delete();
-
-        Schema::disableForeignKeyConstraints();
-        foreach ($curso->qualifications()->get() as $qualification) {
-            $qualification->skills()->sync([]);
-        }
-        $curso->qualifications()->delete();
-        $curso->skills()->delete();
-        Schema::enableForeignKeyConstraints();
-
-        $curso->feedbacks()->delete();
-
-        $curso->milestones()->delete();
-
-        foreach ($curso->hilos()->get() as $hilo) {
-            DB::table('messages')
-                ->where('thread_id', '=', $hilo->id)
-                ->delete();
-
-            DB::table('participants')
-                ->where('thread_id', '=', $hilo->id)
-                ->delete();
-        }
-
-        $curso->hilos()->delete();
-
-        $curso->groups()->detach();
-
-        Schema::disableForeignKeyConstraints();
-        $curso->delete();
-        Schema::enableForeignKeyConstraints();
-
+        BorrarCurso::dispatch($curso);
         return back();
     }
 
---
+++ app/Jobs/BorrarCurso.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Curso;
+use Ikasgela\Gitea\GiteaClient;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+
+class BorrarCurso implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected Curso $curso;
+
+    public function __construct(Curso $curso)
+    {
+        $this->onQueue('low');
+
+        $this->curso = $curso;
+    }
+
+    public function handle()
+    {
+        $curso = $this->curso;
+
+        Log::debug('Iniciando borrado de curso...', [
+            'curso' => $curso->slug
+        ]);
+
+        foreach ($curso->actividades()->get() as $actividad) {
+            DB::table('tareas')
+                ->where('actividad_id', '=', $actividad->id)
+                ->delete();
+
+            DB::table('actividad_team')
+                ->where('actividad_id', '=', $actividad->id)
+                ->delete();
+
+            $actividad->feedbacks()->delete();
+        }
+
+        DB::table('curso_user')
+            ->where('curso_id', '=', $curso->id)
+            ->delete();
+
+        DB::table('settings')
+            ->where('key', '=', 'curso_actual')
+            ->where('value', '=', $curso->id)
+            ->delete();
+
+        $curso->intellij_projects()->delete();
+        GiteaClient::borrar_organizacion($curso->slug);
+        $curso->markdown_texts()->delete();
+        $curso->youtube_videos()->delete();
+        $curso->cuestionarios()->delete();
+
+        foreach ($curso->file_resources_files as $file) {
+            $file->delete();
+        }
+        $curso->file_resources()->delete();
+
+        foreach ($curso->file_uploads_files as $file) {
+            $file->delete();
+        }
+        $curso->file_uploads()->delete();
+
+        $curso->link_collections_links()->delete();
+        $curso->link_collections()->delete();
+
+        Schema::disableForeignKeyConstraints();
+        $curso->actividades()->forceDelete();
+        Schema::enableForeignKeyConstraints();
+
+        $curso->unidades()->delete();
+
+        Schema::disableForeignKeyConstraints();
+        foreach ($curso->qualifications()->get() as $qualification) {
+            $qualification->skills()->sync([]);
+        }
+        $curso->qualifications()->delete();
+        $curso->skills()->delete();
+        Schema::enableForeignKeyConstraints();
+
+        $curso->feedbacks()->delete();
+
+        $curso->milestones()->delete();
+
+        foreach ($curso->hilos()->get() as $hilo) {
+            DB::table('messages')
+                ->where('thread_id', '=', $hilo->id)
+                ->delete();
+
+            DB::table('participants')
+                ->where('thread_id', '=', $hilo->id)
+                ->delete();
+        }
+
+        $curso->hilos()->delete();
+
+        $curso->groups()->detach();
+
+        Schema::disableForeignKeyConstraints();
+        $curso->delete();
+        Schema::enableForeignKeyConstraints();
+
+        Log::debug('Curso borrado.', [
+            'curso' => $curso->slug
+        ]);
+    }
+}
