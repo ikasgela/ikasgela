b3a19777855932b8a97b281e25052fd50ba25730
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-07-20 23:42:33 +0200

    Página de presentación revisada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,23 +4,20 @@
 
     <div class="row">
         <div class="col-12 col-lg-6 mx-auto">
-            {{-- Tarjeta --}}
             <div class="card">
                 <div class="card-header">{{ __('Welcome') }}</div>
                 <div class="card-body pb-1">
                     <p>{{ __('You need a user account to use ikasgela.') }}</p>
                 </div>
                 <hr class="mt-0 mb-0">
-                <div class="card-body pb-4 text-center">
-                    <a class="btn btn-primary mr-3" href="{{ route('login') }}">{{ __('Sign in') }}</a>
+                <div class="card-body pb-3 text-center">
+                    <a class="btn btn-primary me-3" href="{{ route('login') }}">{{ __('Sign in') }}</a>
                     @if(!is_null($current_organization) && $current_organization->isRegistrationOpen())
                         <span class="mx-3">{{ __('or') }}</span>
                         <a class="btn btn-link" href="{{ route('register') }}">{{ __('Sign up') }}</a>
                     @endif
                 </div>
             </div>
-            {{-- Fin tarjeta--}}
-
         </div>
     </div>
 
