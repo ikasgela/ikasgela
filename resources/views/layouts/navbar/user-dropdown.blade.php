bd92ee4c5d07e6aa81f5f317248a67a0d29ccc75
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-07-20 23:17:55 +0200

    Centrado vertical del botón de despliegue

--- resources/views/layouts/navbar/user-dropdown.blade.php
+++ resources/views/layouts/navbar/user-dropdown.blade.php
@@ -1,8 +1,8 @@
 <li class="nav-item dropdown">
     <a id="navbarDropdown"
-       class="nav-link dropdown-toggle text-{{ $debug_text_color }}" href="#"
+       class="nav-link dropdown-toggle text-{{ $debug_text_color }} d-flex align-items-center" href="#"
        role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {{ Auth::user()->name }}
+        <span class="me-1">{{ Auth::user()->name }}</span>
     </a>
     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
         <a class="dropdown-item" href="{{ route('logout') }}"
