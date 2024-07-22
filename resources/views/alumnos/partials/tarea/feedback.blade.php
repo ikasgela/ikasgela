914e9839496f0de4b9d0cf7ee881a4c61ae5f031
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-07-22 14:50:15 +0200

    Formato del feedback revisado

--- resources/views/alumnos/partials/tarea/feedback.blade.php
+++ resources/views/alumnos/partials/tarea/feedback.blade.php
@@ -1,16 +1,14 @@
 @if($actividad->tarea->estado > 10 && $actividad->tarea->estado != 30)
     @if(!is_null($actividad->tarea->feedback))
         <hr class="mt-0 mb-2">
-        <div class="row mt-3 mb-0 mx-2">
+        <div class="row my-3 mx-2">
             <div class="col-md-12">
                 <div class="card
                             {{ $actividad->unidad->curso->disponible() && $actividad->tarea->estado == 40 ? !$actividad->hasEtiqueta('examen') ? 'border-success' : 'border-secondary' : '' }}
-                            {{ $actividad->unidad->curso->disponible() && $actividad->tarea->estado == 41 ? 'border-warning' : '' }}
-                            {{ !$actividad->unidad->curso->disponible() ? 'border-secondary' : '' }}">
+                            {{ $actividad->unidad->curso->disponible() && $actividad->tarea->estado == 41 ? 'border-warning' : '' }}">
                     <div class="card-header
-                                {{ $actividad->unidad->curso->disponible() && $actividad->tarea->estado == 40 ? !$actividad->hasEtiqueta('examen') ? 'bg-success' : 'bg-secondary' : '' }}
-                                {{ $actividad->unidad->curso->disponible() && $actividad->tarea->estado == 41 ? 'bg-warning text-dark' : '' }}
-                                {{ !$actividad->unidad->curso->disponible() ? 'bg-secondary' : '' }}">
+                                {{ $actividad->unidad->curso->disponible() && $actividad->tarea->estado == 40 ? !$actividad->hasEtiqueta('examen') ? 'text-bg-success' : 'text-bg-secondary' : '' }}
+                                {{ $actividad->unidad->curso->disponible() && $actividad->tarea->estado == 41 ? 'text-bg-warning' : '' }}">
                         <i class="fas fa-bullhorn"></i> {{ __('Feedback') }}
                     </div>
                     <div class="mx-3 mt-3 p-1">
