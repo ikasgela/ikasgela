dfc239f716bc5a47ec93e74f75ee39d56fb2a2fc
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-07-23 23:34:01 +0200

    Menú desplegable del usuario

--- resources/views/layouts/navbar/user-dropdown.blade.php
+++ resources/views/layouts/navbar/user-dropdown.blade.php
@@ -10,9 +10,37 @@
         @endif
     </a>
     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+        <div class="dropdown-item-text">
+            <h5>{{ Auth::user()->full_name }}</h5>
+            <small class="text-body-tertiary">{{ Auth::user()->email }}</small>
+        </div>
+        <div class="dropdown-divider"></div>
+        @include('layouts.partials.toggle_help')
+        @if(Auth::user()->hasAnyRole(['alumno','profesor']))
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="{{ route('notifications.edit') }}">
+                <span class="text-center ms-n2 me-1" style="width: 1.5rem;">
+                    <i class="fas fa-bell text-primary"></i>
+                </span> {{ __('Notification settings') }}
+            </a>
+        @endif
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="{{ url('/profile') }}">
+            <span class="text-center ms-n2 me-1" style="width: 1.5rem;">
+                <i class="fas fa-user text-primary"></i>
+            </span> {{ __('Profile') }}
+        </a>
+        <a class="dropdown-item" href="{{ url('/password') }}">
+            <span class="text-center ms-n2 me-1" style="width: 1.5rem;">
+                <i class="fas fa-key text-primary"></i>
+            </span> {{ __('Password') }}
+        </a>
+        <div class="dropdown-divider"></div>
         <a class="dropdown-item" href="{{ route('logout') }}"
            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}
+            <span class="text-center ms-n2 me-1" style="width: 1.5rem;">
+                <i class="fas fa-sign-out-alt"></i>
+            </span> {{ __('Logout') }}
         </a>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
             @csrf
