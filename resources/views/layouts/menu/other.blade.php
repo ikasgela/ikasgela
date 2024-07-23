6701878b8c1386f2474ee41db385b077ad6032c6
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-07-23 15:19:46 +0200

    Menús de la plantilla borrados

--- resources/views/layouts/menu/other.blade.php
+++
@@ -1,9 +0,0 @@
-@include('layouts.sidebar.nav-title', [
-    'text' => 'Other',
-])
-@include('layouts.sidebar.nav-item', [
-    'route' => '#',
-    'text' => 'Test',
-    'icon' => 'bi-compass',
-    'last' => true,
-])
