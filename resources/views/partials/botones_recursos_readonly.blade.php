f750b108d89680ec2c4e35325c3547c3e99c888f
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-07-27 01:36:20 +0200

    Archivo revisado

--- resources/views/partials/botones_recursos_readonly.blade.php
+++ resources/views/partials/botones_recursos_readonly.blade.php
@@ -1,30 +1,30 @@
 <div class='btn-group'>
     <button title="{{ __('Markdown text') }}"
-            class="btn btn-sm {{ $actividad->markdown_texts()->count() > 0 ? 'btn-secondary' : 'btn-light' }}">
+            class="btn btn-sm {{ $actividad->markdown_texts()->count() > 0 ? 'btn-secondary' : 'bg-body-secondary' }}">
         <i class="fab fa-markdown"></i>
     </button>
     <button title="{{ __('YouTube video') }}"
-            class="btn btn-sm {{ $actividad->youtube_videos()->count() > 0 ? 'btn-secondary' : 'btn-light' }}">
+            class="btn btn-sm {{ $actividad->youtube_videos()->count() > 0 ? 'btn-secondary' : 'bg-body-secondary' }}">
         <i class="fab fa-youtube"></i>
     </button>
     <a title="{{ __('Links') }}"
-       class="btn btn-sm {{ $actividad->link_collections()->count() > 0 ? 'btn-secondary' : 'btn-light' }}">
+       class="btn btn-sm {{ $actividad->link_collections()->count() > 0 ? 'btn-secondary' : 'bg-body-secondary' }}">
         <i class="fas fa-link"></i>
     </a>
     <a title="{{ __('Files') }}"
-       class="btn btn-sm {{ $actividad->file_resources()->count() > 0 ? 'btn-secondary' : 'btn-light' }}">
+       class="btn btn-sm {{ $actividad->file_resources()->count() > 0 ? 'btn-secondary' : 'bg-body-secondary' }}">
         <i class="fas fa-file"></i>
     </a>
     <button title="{{ __('Questionnaire') }}"
-            class="btn btn-sm {{ $actividad->cuestionarios()->count() > 0 ? 'btn-secondary' : 'btn-light' }}">
+            class="btn btn-sm {{ $actividad->cuestionarios()->count() > 0 ? 'btn-secondary' : 'bg-body-secondary' }}">
         <i class="fas fa-question-circle"></i>
     </button>
     <button title="{{ __('Image upload') }}"
-            class="btn btn-sm {{ $actividad->file_uploads()->count() > 0 ? 'btn-secondary' : 'btn-light' }}">
+            class="btn btn-sm {{ $actividad->file_uploads()->count() > 0 ? 'btn-secondary' : 'bg-body-secondary' }}">
         <i class="fas fa-file-upload"></i>
     </button>
     <button title="{{ __('IntelliJ project') }}"
-            class="btn btn-sm {{ $actividad->intellij_projects()->count() > 0 ? 'btn-secondary' : 'btn-light' }}">
+            class="btn btn-sm {{ $actividad->intellij_projects()->count() > 0 ? 'btn-secondary' : 'bg-body-secondary' }}">
         <i class="fab fa-java"></i>
     </button>
 </div>
