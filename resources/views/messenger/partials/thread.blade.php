d82f89662524248b60adb6537888f84b443488b1
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-07-26 13:09:18 +0200

    Mostrar o no los avatares en los mensajes

--- resources/views/messenger/partials/thread.blade.php
+++ resources/views/messenger/partials/thread.blade.php
@@ -14,30 +14,44 @@
         </span>
         <span>{{ $thread->userUnreadMessagesCount(Auth::id()) }} {{ __('unread') }}</span>
     </div>
-    <div class="card-body pb-1">
-        <div class="d-flex justify-content-between">
-            <h5 class="card-title"><a href="{{ route('messages.show', $thread->id) }}">{{ $thread->subject }}</a></h5>
-            @auth
-                @if(Auth::user()->hasRole('profesor'))
-                    {!! Form::open(['route' => ['messages.destroy', $thread->id], 'method' => 'DELETE']) !!}
-                    <div class="btn-group">
-                        @include('partials.boton_borrar')
-                    </div>
-                    {!! Form::close() !!}
-                @endif
-            @endauth
-        </div>
-    </div>
-    @if(!is_null($thread->latestMessage))
-        <small class="text-secondary mx-3 mb-1">{{ __('Latest message') }}</small>
-        <div class="text-body bg-light-subtle mx-3 mb-3 line-numbers">
-            <div class="px-3 pt-3 overflow-auto border-start border-secondary-subtle border-4">
-                {!! links_galeria($thread->latestMessage->body, $thread->id) !!}
+
+    @if(config('ikasgela.avatar_enabled'))
+        <div class="d-flex align-items-start">
+            <div class="ps-3 pt-3">
+                @include('users.partials.avatar', ['user' => $thread->creator(), 'width' => 64])
             </div>
+            @endif()
+
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <h5 class="card-title"><a
+                            href="{{ route('messages.show', $thread->id) }}">{{ $thread->subject }}</a>
+                    </h5>
+                    @auth
+                        @if(Auth::user()->hasRole('profesor'))
+                            {!! Form::open(['route' => ['messages.destroy', $thread->id], 'method' => 'DELETE']) !!}
+                            <div class="btn-group">
+                                @include('partials.boton_borrar')
+                            </div>
+                            {!! Form::close() !!}
+                        @endif
+                    @endauth
+                </div>
+                @if(!is_null($thread->latestMessage))
+                    <small class="text-secondary mb-1">{{ __('Latest message') }}</small>
+                    <div class="text-body bg-light-subtle mb-3 line-numbers">
+                        <div class="px-3 pt-3 overflow-auto border-start border-secondary-subtle border-4">
+                            {!! links_galeria($thread->latestMessage->body, $thread->id) !!}
+                        </div>
+                    </div>
+                    <small class="text-secondary"
+                           title="{{ $thread->latestMessage->created_at->isoFormat('dddd, LL LTS') }}">
+                        {{ __('Posted') }} {{ $thread->latestMessage->created_at->diffForHumans() }}
+                    </small>
+                @endif
+            </div>
+
+            @if(config('ikasgela.avatar_enabled'))
         </div>
-        <small class="text-secondary mx-3 mb-3"
-               title="{{ $thread->latestMessage->created_at->isoFormat('dddd, LL LTS') }}">
-            {{ __('Posted') }} {{ $thread->latestMessage->created_at->diffForHumans() }}
-        </small>
     @endif
 </div>
