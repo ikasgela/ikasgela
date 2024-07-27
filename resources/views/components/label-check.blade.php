81539a58e3c0785800a32b7a0a11c3e969c75165
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-07-27 21:24:03 +0200

    Ajustes de notificaciones

---
+++ resources/views/components/label-check.blade.php
@@ -0,0 +1,8 @@
+<div class="row mb-3">
+    <div class="col-sm-2">
+        {{ html()->label($label, $name)->class('form-check-label') }}
+    </div>
+    <div class="col-sm-10">
+        {{ html()->checkbox($name, $checked)->class('form-check-input') }}
+    </div>
+</div>
