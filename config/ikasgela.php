151a4c2cb18a7f25321b382aa8502cd630ad64b2
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-08-01 00:40:26 +0200

    Convertir los enteros a valor numérico

--- config/ikasgela.php
+++ config/ikasgela.php
@@ -2,9 +2,9 @@
 
 return [
 
-    'repo_cache_days' => env('REPO_CACHE_DAYS', 7),
-    'markdown_cache_days' => env('MARKDOWN_CACHE_DAYS', 7),
-    'eloquent_cache_time' => env('ELOQUENT_CACHE_TIME', 7200),
+    'repo_cache_days' => intval(env('REPO_CACHE_DAYS', 7)),
+    'markdown_cache_days' => intval(env('MARKDOWN_CACHE_DAYS', 7)),
+    'eloquent_cache_time' => intval(env('ELOQUENT_CACHE_TIME', 7200)),
 
     'gitlab_enabled' => env('GITLAB_ENABLED', false),
     'gitea_enabled' => env('GITEA_ENABLED', false),
@@ -14,10 +14,10 @@ return [
 
     'tinymce_apikey' => env('TINYMCE_APIKEY', 'none'),
 
-    'message_preview_max_length' => env('MESSAGE_PREVIEW_MAX_LENGTH', 300),
+    'message_preview_max_length' => intval(env('MESSAGE_PREVIEW_MAX_LENGTH', 300)),
 
-    'pagination_assigned_activities' => env('PAGINATION_ASSIGNED_ACTIVITIES', 20),
-    'pagination_available_activities' => env('PAGINATION_AVAILABLE_ACTIVITIES', 10),
+    'pagination_assigned_activities' => intval(env('PAGINATION_ASSIGNED_ACTIVITIES', 20)),
+    'pagination_available_activities' => intval(env('PAGINATION_AVAILABLE_ACTIVITIES', 10)),
 
     'pdf_report_enabled' => env('PDF_REPORT_ENABLED', false),
     'excel_report_enabled' => env('EXCEL_REPORT_ENABLED', false),
