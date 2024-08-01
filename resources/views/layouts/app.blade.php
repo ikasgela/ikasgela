76e5c2547ace0cd4249eab5d71e260940a5379e8
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-08-01 03:39:40 +0200

    Añadir el fondo de color a la cabecera de iOS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="theme-color" content="{{ config('app.debug') ? '#ffc107' : '#3490dc' }}">
     <title>
         {{ config('app.name', 'Laravel') }}
         {{ subdominio() != 'ikasgela' ? ' | '. subdominio() :  '' }}
