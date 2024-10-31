96c0217b43113796309c1231acc1e1ca7eecc90e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-10-31 11:12:45 +0100

    Icono para salir del modo suplantar usuario

---
+++ resources/views/layouts/navbar/user-impersonated.blade.php
@@ -0,0 +1,8 @@
+<li class="nav-link d-sm-inline-flex">
+    <a title="{{ __('Leave impersonation') }}"
+       style="width:24px;height:24px;font-size:10px;line-height:24px;"
+       href="{{ route('impersonate.leave') }}"
+       class="btn btn-sm btn-outline-{{ $debug_text_color }} p-0">
+        <i class="fas fa-user-secret"></i>
+    </a>
+</li>
