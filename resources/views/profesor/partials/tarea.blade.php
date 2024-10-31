7afbec2204fee0977b7bf1c1854d843f4fe8d757
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-10-31 16:04:23 +0100

    Botonera de calificación revisada

--- resources/views/profesor/partials/tarea.blade.php
+++ resources/views/profesor/partials/tarea.blade.php
@@ -21,74 +21,7 @@
                 <div class="card-body">
                     @include('actividades.partials.encabezado_con_etiquetas')
                     <p>{{ $actividad->descripcion }}</p>
-                    <div class="form-inline">
-                        @if($tarea->estado == 11)
-                            <button type="submit" name="nuevoestado" value="10"
-                                    class="btn btn-secondary single_click">
-                                <i class="fas fa-spinner fa-spin" style="display:none;"></i> {{ __('Show') }}
-                            </button>
-                        @else
-                            <button type="submit" name="nuevoestado" value="31"
-                                    class="btn btn-secondary single_click">
-                                <i class="fas fa-spinner fa-spin" style="display:none;"></i> {{ __('Reset') }}
-                            </button>
-                        @endif
-                        <label class="mx-3">{{ __('Attempts') }}: {{ $tarea->intentos }}</label>
-                        <div class="form-inline">
-                            <button type="submit" name="nuevoestado" value="41"
-                                    class="me-3 btn btn-warning single_click"
-                                    onclick="return validate_feedback();">
-                                <i class="fas fa-spinner fa-spin" style="display:none;"></i> {{ __('Send again') }}
-                            </button>
-                            <label class="me-2">{{ __('Score') }}</label>
-                            <input class="me-2 form-control" type="number" min="0" max="100" step="1"
-                                   name="puntuacion"
-                                   value="{{ !is_null($tarea->puntuacion) ? $tarea->puntuacion : $actividad->puntuacion }}"/>
-                            <label class="me-3"> {{ __('over') }} {{ $actividad->puntuacion }}</label>
-                            <button type="submit" name="nuevoestado" value="40"
-                                    class="btn btn-primary me-3 single_click"
-                                    onclick="return validate_feedback();">
-                                <i class="fas fa-spinner fa-spin" style="display:none;"></i> {{ __('Finished') }}
-                            </button>
-                            @if($actividad->is_expired)
-                                <button type="submit" name="nuevoestado" value="62"
-                                        class="btn btn-secondary ms-3 single_click">
-                                    <i class="fas fa-spinner fa-spin"
-                                       style="display:none;"></i> {{ __('Archive expired') }}
-                                </button>
-                                <button type="submit" name="nuevoestado" value="63"
-                                        class="btn btn-secondary ms-3 single_click">
-                                    <i class="fas fa-spinner fa-spin"
-                                       style="display:none;"></i> {{ __('Extend deadline') }}
-                                </button>
-                                <label class="mx-2">{{ __('by') }}</label>
-                                <input class="me-2 form-control" type="number" min="0" max="90" step="1"
-                                       name="ampliacion_plazo"
-                                       value="{{ $actividad->unidad->curso->plazo_actividad ?? 7 }}"/>
-                                <label class="me-2">{{ __('days') }}.</label>
-                            @endif
-                        </div>
-                        @if(!is_null($actividad->siguiente))
-                            <div class="form-inline">
-                                <label>{{ __('Next') }}: @include('actividades.partials.siguiente')</label>
-                                @if($actividad->final)
-                                    <button type="submit" name="nuevoestado" value="70"
-                                            class="mx-3 btn btn-light single_click">
-                                        <i class="fas fa-spinner fa-spin" style="display:none;"></i> {{ __('Resume') }}
-                                    </button>
-                                @else
-                                    <button type="submit" name="nuevoestado" value="70"
-                                            class="mx-3 btn btn-light single_click">
-                                        <i class="fas fa-spinner fa-spin" style="display:none;"></i> {{ __('Pause') }}
-                                    </button>
-                                @endif
-                                <button type="submit" name="nuevoestado" value="71"
-                                        class="btn btn-light single_click">
-                                    <i class="fas fa-spinner fa-spin" style="display:none;"></i> {{ __('Show next') }}
-                                </button>
-                            </div>
-                        @endif()
-                    </div>
+                    @include('profesor.partials.botonera-calificacion')
                     <div class="mt-2">
                         <i class="fas fa-bullhorn mt-3"></i>
                         <label class="m-0" for="feedback">{{ __('Feedback') }}</label>
