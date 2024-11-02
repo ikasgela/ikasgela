34d464eb70d6f4acc71196b908f36855923576f7
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-02 18:44:42 +0100

    Permitir descargar el fichero de SEB

--- resources/views/safe_exam/index.blade.php
+++ resources/views/safe_exam/index.blade.php
@@ -29,7 +29,7 @@
                             <button title="{{ __('Reset token') }}"
                                     name="reset_token"
                                     type="submit" onclick="return confirm('{{ __('Are you sure?') }}')"
-                                    class="btn btn-light btn-sm"><i class="fas fa-redo text-danger"></i>
+                                    class="btn btn-light btn-sm me-2"><i class="fas fa-redo text-danger"></i>
                             </button>
                             {{ html()->form()->close() }}
                             {{ html()->form('DELETE', route('safe_exam.delete_token', $curso->id))->open() }}
@@ -50,7 +50,7 @@
                             <button title="{{ __('Reset quit password') }}"
                                     name="reset_quit_password"
                                     type="submit" onclick="return confirm('{{ __('Are you sure?') }}')"
-                                    class="btn btn-light btn-sm"><i class="fas fa-redo text-danger"></i>
+                                    class="btn btn-light btn-sm me-2"><i class="fas fa-redo text-danger"></i>
                             </button>
                             {{ html()->form()->close() }}
                             {{ html()->form('DELETE', route('safe_exam.delete_quit_password', $curso->id))->open() }}
@@ -63,6 +63,11 @@
                         </div>
                     </td>
                     <td>
+                        <a href="{{ route('safe_exam.config_seb', [$curso->safe_exam?->id ?: App\Models\SafeExam::create(['curso_id' => $curso->id])->id]) }}"
+                           title="{{ __('Download SEB configuration file') }}"
+                           class="btn btn-sm btn-light me-2" role="button">
+                            <i class="bi bi-download"></i>
+                        </a>
                         <a href="{{ route('safe_exam.allowed', [$curso->safe_exam?->id ?: App\Models\SafeExam::create(['curso_id' => $curso->id])->id]) }}"
                            title="{{ __('Allowed apps and URLs') }}"
                            class="btn btn-sm btn-light" role="button">
