e6bfb0d800d342c5dcd5d9691be3f8dc43e3acd0
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-05 00:18:56 +0100

    Formulario mejorado

--- resources/views/profesor/partials/disponibles.blade.php
+++ resources/views/profesor/partials/disponibles.blade.php
@@ -7,19 +7,22 @@
         @csrf
         @include('profesor.partials.tabla_disponibles')
         @include('layouts.errors')
-        <div class="d-flex flex-row flex-wrap justify-content-between align-items-baseline mb-4">
+        <div class="d-flex justify-content-between mb-4">
             <div>
                 <button type="submit" class="btn btn-primary single_click">
                     <i class="fas fa-spinner fa-spin" style="display:none;"></i> {{ __('Save assigment') }}
                 </button>
                 <a href="{{ route('profesor.index') }}" class="btn btn-link text-secondary">{{ __('Cancel') }}</a>
             </div>
-            <div>
-                <input type="checkbox" id="fecha_override_enable" name="fecha_override_enable"/>
-                <label class="ml-1" for="fecha_override_enable">{{ __('Override completion date') }}</label>
-                <input class="mx-2" type="text" id="fecha_override" name="fecha_override"/>
-                <input type="checkbox" id="notificar" name="notificar" checked="checked"/>
-                <label class="ml-1" for="notificar">{{ __('Send notification email') }}</label>
+            <div class="d-flex justify-content-end align-items-center" style="flex-grow: 2">
+                <input class="form-check-input me-2 my-0" type="checkbox"
+                       id="fecha_override_enable" name="fecha_override_enable"/>
+                <label class="form-check-label" for="fecha_override_enable">{{ __('Override completion date') }}</label>
+                <input class="form-control ms-2 me-3" style="flex-basis: 16em" type="text"
+                       id="fecha_override" name="fecha_override"/>
+                <input class="form-check-input me-2 my-0" type="checkbox" id="notificar" name="notificar"
+                       checked="checked"/>
+                <label class="form-check-label" for="notificar">{{ __('Send notification email') }}</label>
             </div>
         </div>
     </form>
