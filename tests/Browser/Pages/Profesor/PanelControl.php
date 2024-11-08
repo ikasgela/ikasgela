d91e7f1091892fdfc05b09c1fee105152ebe33eb
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-08 13:45:01 +0100

    Nuevos tests

---
+++ tests/Browser/Pages/Profesor/PanelControl.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Browser\Pages\Profesor;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class PanelControl extends DuskTestCase
+{
+    public function testLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('login'));
+            $browser->type('email', 'lucia@ikasgela.com');
+            $browser->type('password', '12345Abcde');
+            $browser->check('remember');
+            $browser->press(__('Login'));
+            $browser->assertRouteIs('profesor.index');
+        });
+    }
+
+    public function testPanelControl()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('profesor.index'));
+            $browser->assertRouteIs('profesor.index');
+
+            if (config('ikasgela.avatar_enabled')) {
+                $browser->assertSeeIn('main > div.p-4.col-12.col-sm-10 > div.table-responsive > table > tbody > tr:nth-child(1) > td:nth-child(4)', 'Noa');
+                $browser->assertSeeIn('main > div.p-4.col-12.col-sm-10 > div.table-responsive > table > tbody > tr:nth-child(2) > td:nth-child(4)', 'Marc');
+            } else {
+                $browser->assertSeeIn('main > div.p-4.col-12.col-sm-10 > div.table-responsive > table > tbody > tr:nth-child(1) > td:nth-child(3)', 'Noa');
+                $browser->assertSeeIn('main > div.p-4.col-12.col-sm-10 > div.table-responsive > table > tbody > tr:nth-child(2) > td:nth-child(3)', 'Marc');
+            }
+        });
+    }
+
+    public function testLogout()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->logout();
+            $browser->visit(route('portada'));
+            $browser->assertRouteIs('portada');
+        });
+    }
+}
---
+++ tests/Browser/Pages/Profesor/Tutoria.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Browser\Pages\Profesor;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class Tutoria extends DuskTestCase
+{
+    public function testLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('login'));
+            $browser->type('email', 'lucia@ikasgela.com');
+            $browser->type('password', '12345Abcde');
+            $browser->check('remember');
+            $browser->press(__('Login'));
+            $browser->assertRouteIs('profesor.index');
+        });
+    }
+
+    public function testTutoria()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('messages'));
+            $browser->assertRouteIs('messages');
+            $browser->assertSee(__('Tutorship'));
+        });
+    }
+
+    public function testLogout()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->logout();
+            $browser->visit(route('portada'));
+            $browser->assertRouteIs('portada');
+        });
+    }
+}
