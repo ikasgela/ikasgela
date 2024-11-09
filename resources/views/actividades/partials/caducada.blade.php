aaeea6e57c6cf78ab93f3df5cb55595871a2279d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-09 11:23:50 +0100

    Vista de plantillas más compacta

--- resources/views/actividades/partials/caducada.blade.php
+++ resources/views/actividades/partials/caducada.blade.php
@@ -1,7 +1,7 @@
 @if($actividad->is_finished || $actividad->is_expired)
     @if($actividad->plantilla)
-        <i class="fas fa-exclamation-triangle text-secondary" title="{{ trans_choice('tasks.expired', 1) }}"></i>
+        <i class="fas fa-exclamation-triangle text-secondary ms-2" title="{{ trans_choice('tasks.expired', 1) }}"></i>
     @else
-        <i class="fas fa-exclamation-triangle text-warning" title="{{ trans_choice('tasks.expired', 1) }}"></i>
+        <i class="fas fa-exclamation-triangle text-warning ms-2" title="{{ trans_choice('tasks.expired', 1) }}"></i>
     @endif
 @endif
--- resources/views/actividades/partials/tabla_actividades.blade.php
+++ resources/views/actividades/partials/tabla_actividades.blade.php
@@ -11,14 +11,12 @@
             @if(Route::currentRouteName() == 'actividades.index')
                 <th></th>
             @endif
-            <th>{{ __('Course') }}</th>
             <th>{{ __('Unit') }}</th>
             @if(Str::startsWith(Route::currentRouteName(), 'actividades.plantillas'))
                 <th>{{ __('Order') }}</th>
             @endif
             <th>{{ __('Name') }}</th>
-            <th>{{ __('Slug') }}</th>
-            <th>{{ __('Score') }}</th>
+            <th class="text-center">{{ __('Score') }}</th>
             <th class="text-center">{{ __('Auto') }}</th>
             <th>{{ __('Next') }}</th>
             <th colspan="2">{{ __('Resources') }}</th>
@@ -38,7 +36,6 @@
                 @if(Route::currentRouteName() == 'actividades.index')
                     <td>{!! $actividad->plantilla ? '<i class="fas fa-file"></i>' : '' !!}</td>
                 @endif
-                <td class="clickable">{{ $actividad->unidad->curso->full_name }}</td>
                 <td class="clickable">{{ $actividad->unidad->full_name }}</td>
                 @if(Str::startsWith(Route::currentRouteName(), 'actividades.plantillas'))
                     <td>
@@ -47,7 +44,7 @@
                                     form="multiple"
                                     type="submit"
                                     name="action" value="mm_{{ $actividad->id }}"
-                                    class="btn btn-light btn-sm me-3">
+                                    class="btn btn-light btn-sm me-3 rounded">
                                 <i class="fas fa-arrow-right"></i>
                             </button>
                             {{ html()->form('POST', route('actividades.reordenar', [$ids[$loop->index], $ids[$loop->index-1] ?? -1]))->open() }}
@@ -70,11 +67,15 @@
                     </td>
                 @endif
                 <td class="clickable">
-                    @include('actividades.partials.nombre_con_etiquetas')
-                    @include('actividades.partials.caducada')
+                    <div>
+                        <div class="d-flex align-items-center">
+                            @include('actividades.partials.nombre_con_etiquetas')
+                            @include('actividades.partials.caducada')
+                        </div>
+                        <span class="small text-secondary">{{ $actividad->slug }}</span>
+                    </div>
                 </td>
-                <td class="clickable">{{ $actividad->slug }}</td>
-                <td class="clickable">{{ formato_decimales($actividad->puntuacion * ($actividad->multiplicador ?: 1)) }}</td>
+                <td class="text-center clickable">{{ formato_decimales($actividad->puntuacion * ($actividad->multiplicador ?: 1)) }}</td>
                 <td class="text-center clickable">@include('partials.check_yes_no', ['checked' => $actividad->auto_avance])</td>
                 <td class="clickable">
                     @include('actividades.partials.siguiente')
@@ -98,11 +99,11 @@
                         {{ html()->form('POST', route('actividades.duplicar', $actividad->id))->open() }}
                         <button title="{{ __('Duplicate') }}"
                                 type="submit"
-                                class="btn btn-light btn-sm"><i class="fas fa-copy"></i>
+                                class="btn btn-light btn-sm rounded-0"><i class="fas fa-copy"></i>
                         </button>
                         {{ html()->form()->close() }}
                         {{ html()->form('DELETE', route('actividades.destroy', $actividad->id))->open() }}
-                        @include('partials.boton_borrar')
+                        @include('partials.boton_borrar', ['last' => true])
                         {{ html()->form()->close() }}
                     </div>
                 </td>
