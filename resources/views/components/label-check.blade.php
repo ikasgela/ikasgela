4c6a1f014f43a6b3e56641bc13b28b49bd56c67f
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-10 19:55:42 +0100

    Componente select nuevo

--- resources/views/components/label-check.blade.php
+++ resources/views/components/label-check.blade.php
@@ -3,6 +3,6 @@
         {{ html()->label($label, $name)->class('form-check-label') }}
     </div>
     <div class="col-sm-10">
-        {{ html()->checkbox($name, $checked)->class('form-check-input') }}
+        {{ html()->checkbox($name, $checked ?? false)->class('form-check-input') }}
     </div>
 </div>
