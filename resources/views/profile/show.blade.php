a3f4c90342338b3b38ed4f6cccf2e14f0c17bb89
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-12 00:02:32 +0100

    Alinear el avatar arriba

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -28,7 +28,7 @@
             ])
 
             <div class="row mb-3">
-                <div class="col-sm-2 d-flex align-items-center">
+                <div class="col-sm-2">
                     {{ html()->label(__('Avatar'), 'avatar')->class('form-label') }}
                 </div>
                 <div class="col-sm-10">
