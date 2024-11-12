4d79f0a9af07d473ade5540c55cd49c16296f269
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-12 08:56:20 +0100

    Editar una tarea

--- resources/views/tareas/edit.blade.php
+++ resources/views/tareas/edit.blade.php
@@ -1,23 +1,49 @@
 @extends('layouts.app')
 
+@section('tinymce')
+    @include('profesor.partials.tinymce')
+@endsection
+
 @section('content')
 
     @include('partials.titular', ['titular' => __('Edit task')])
 
-    <div class="card">
+    <div class="card mb-3">
         <div class="card-body">
 
-            {!! Form::model($tarea, ['route' => ['tareas.update', $tarea->id], 'method' => 'PUT']) !!}
+            {{ html()->modelForm($tarea, 'PUT', route('tareas.update', $tarea->id))->open() }}
 
-            {{ Form::campoTexto('estado', __('Status')) }}
-            {{ Form::campoTextArea('feedback', __('Feedback')) }}
-            {{ Form::campoTexto('puntuacion', __('Score')) }}
-            {{ Form::campoTexto('intentos', __('Attempts')) }}
+            @include('components.label-text', [
+                'label' => __('Status'),
+                'name' => 'estado',
+            ])
+
+            <div class="row mb-3">
+                <div class="col-sm-2">
+                    {{ html()->label(__('Feedback'), 'feedback')->class('col-form-label') }}
+                </div>
+                <div class="col-sm-10">
+                    <textarea class="form-control"
+                              id="feedback"
+                              name="feedback"
+                              rows="15">
+                        {{ !is_null($tarea->feedback) ? $tarea->feedback : '' }}
+                    </textarea>
+                </div>
+            </div>
+
+            @include('components.label-text', [
+                'label' => __('Score'),
+                'name' => 'puntuacion',
+            ])
+            @include('components.label-text', [
+                'label' => __('Attempts'),
+                'name' => 'intentos',
+            ])
 
             @include('partials.guardar_cancelar')
-
             @include('layouts.errors')
-            {!! Form::close() !!}
+            {{ html()->closeModelForm() }}
 
         </div>
     </div>
