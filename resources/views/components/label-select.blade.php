a0bb7acb403f25fc9a6f0dde8712a5a5f290dbdf
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-13 12:51:48 +0100

    Permitir desactivar el select y que no tenga colección

--- resources/views/components/label-select.blade.php
+++ resources/views/components/label-select.blade.php
@@ -3,11 +3,13 @@
         {{ html()->label($label, $name)->class('col-form-label') }}
     </div>
     <div class="col-sm-10">
-        {{ html()->select($name)->class('form-select')->open() }}
+        {{ html()->select($name)->class('form-select')->disabled($disabled ?? false)->open() }}
         {{ isset($default) ? html()->option($default, $default_value ?? null) : '' }}
-        @foreach($coleccion as $elemento)
-            {{ $opcion($elemento) }}
-        @endforeach
+        @isset($coleccion)
+            @foreach($coleccion as $elemento)
+                {{ $opcion($elemento) }}
+            @endforeach
+        @endisset
         {{ html()->select()->close() }}
     </div>
 </div>
