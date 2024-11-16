5e776c5700d96f2a1efdb48bb745bdab62184733
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-11-16 23:56:55 +0100

    Más márgenes revisados

--- resources/views/alumnos/partials/tarea/feedback.blade.php
+++ resources/views/alumnos/partials/tarea/feedback.blade.php
@@ -1,9 +1,9 @@
 @if($actividad->tarea->estado > 10 && $actividad->tarea->estado != 30)
     @if(!is_null($actividad->tarea->feedback))
-        <hr class="mt-0 mb-2">
-        <div class="row my-3 mx-2">
+        <hr class="m-0">
+        <div class="row">
             <div class="col-md-12">
-                <div class="card
+                <div class="card m-3
                             {{ $actividad->unidad->curso->disponible() && $actividad->tarea->estado == 40 ? !$actividad->hasEtiqueta('examen') ? 'border-success' : 'border-secondary' : '' }}
                             {{ $actividad->unidad->curso->disponible() && $actividad->tarea->estado == 41 ? 'border-warning' : '' }}">
                     <div class="card-header
@@ -11,13 +11,13 @@
                                 {{ $actividad->unidad->curso->disponible() && $actividad->tarea->estado == 41 ? 'text-bg-warning' : '' }}">
                         <i class="fas fa-bullhorn"></i> {{ __('Feedback') }}
                     </div>
-                    <div class="mx-3 mt-3 p-1">
+                    <div class="p-3">
                         <div class="media rounded line-numbers">
                             <div class="media-body overflow-auto">
                                 {!! links_galeria($actividad->tarea->feedback) !!}
                             </div>
                         </div>
-                        <hr class="mt-0 mb-2">
+                        <hr class="m-0">
                         <p class="text-muted small">
                             {{ __('Score') }}: @include('actividades.partials.puntuacion')
                         </p>
