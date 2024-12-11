fd436644f11e5d78ac507865a200769b7f7e03c1
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-12-11 11:11:14 +0100

    Traducciones actualizadas

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'email'    => 'Invalid email address.',
     'failed'   => 'These credentials do not match our records.',
-    'password' => 'The password is incorrect.',
+    'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
