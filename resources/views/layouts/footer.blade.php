5606056db9ed1eb2e1c6e1130236a577c8bc7461
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-05-27 19:12:23 +0200

    Componente de prueba

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer mt-auto py-3 bg-secondary-subtle">
     <div class="container text-center">
         <span class="text-secondary">
-            &copy; {{ date('Y') }} Ion Jaureguialzo Sarasola. {{ __('All rights reserved') }}.
+            &copy; {{ config('app.debug') ? now() : date('Y') }} Ion Jaureguialzo Sarasola. {{ __('All rights reserved') }}.
         </span>
     </div>
 </footer>
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,11 +1,12 @@
 <div>
     @include('partials.titular', ['titular' => __('Livewire Test')])
 
-    <h1 class="display-1">{{ $count }}</h1>
+    @include('profesor.partials.tarjeta_usuario')
 
     <button class="btn btn-primary" wire:click="decrement">
         <i class="bi bi-dash-lg"></i>
     </button>
+    <span class="mx-3">{{ $position + 1 }} de {{ $users->count() }}</span>
     <button class="btn btn-primary" wire:click="increment">
         <i class="bi bi-plus-lg"></i>
     </button>
