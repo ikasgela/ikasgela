e1dac7b19543afee9f373ec0eba84621fb6b0f07
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-05-28 11:42:10 +0200

    Evitar excepción si no está definida

--- resources/views/layouts/sidebar/dropdown.blade.php
+++ resources/views/layouts/sidebar/dropdown.blade.php
@@ -2,6 +2,8 @@
     class="btn btn-toggle dropdown-toggle px-3 nav-link collapsed text-light w-100 text-start hover-background"
     data-bs-toggle="collapse"
     data-bs-target="#{{ $collapse_id }}">
-    <i class="bi {{ $icon }} me-2"></i>
+    @isset($icon)
+        <i class="bi {{ $icon }} me-2"></i>
+    @endisset
     <span>{{ $text }}</span>
 </button>
