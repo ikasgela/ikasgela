9041db21c80cfb09997830850290fa11541c114f
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-06-12 01:00:41 +0200

    Quitar uno de los textos

--- resources/views/emails/export_completado.blade.php
+++ resources/views/emails/export_completado.blade.php
@@ -1,8 +1,6 @@
 @component('mail::message')
 # {{ __('Export completed') }}
 
-{{ __('The export is complete.') }}
-
 {{ __('The download link below is valid for 24 hours.') }}
 
 @component('mail::button', ['url' => $url])
