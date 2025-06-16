defb27ebaa6621247adb311cc075071687519f12
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-06-16 17:37:42 +0200

    Modelos y seeder de criterios y grupos

---
+++ app/Models/Criteria.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Bkwld\Cloner\Cloneable;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin IdeHelperCriteria
+ */
+class Criteria extends Model
+{
+    use HasFactory;
+    use Cloneable;
+
+    protected $clone_exempt_attributes = ['seleccionado'];
+
+    protected $fillable = [
+        'texto', 'puntuacion', 'orden', 'seleccionado',
+        'criteria_group_id',
+        '__import_id',
+    ];
+
+    public function criteria_group()
+    {
+        return $this->belongsTo(CriteriaGroup::class);
+    }
+
+    #[Scope]
+    protected function plantilla($query)
+    {
+        return $query->whereHas('criteria_group.rubric', function ($query) {
+            $query->where('plantilla', true);
+        });
+    }
+}
---
+++ database/factories/CriteriaFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CriteriaGroup;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Criteria>
+ */
+class CriteriaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'texto' => fake()->sentence(8),
+            'puntuacion' => 0,
+            'orden' => Str::ulid(),
+            'seleccionado' => false,
+            'criteria_group_id' => CriteriaGroup::factory(),
+        ];
+    }
+}
