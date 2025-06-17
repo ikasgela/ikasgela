f31a1f2d4b7c77fa1b44f904898c78acb4a939cd
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-06-17 08:51:33 +0200

    Usar la sintaxis de livewire

--- resources/views/rubrics/show.blade.php
+++ resources/views/rubrics/show.blade.php
@@ -6,7 +6,7 @@
 
     <div class="row">
         <div class="col-md-12">
-            @livewire('rubric-component', ['rubric' => $rubric])
+            <livewire:rubric-component :$rubric/>
         </div>
     </div>
 
