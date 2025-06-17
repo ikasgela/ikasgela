7f99698bbab2614e621a74390d1143e643c8e45e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-06-17 08:53:57 +0200

    Aspecto consistente

--- resources/views/livewire/rubric-show.blade.php
+++ resources/views/livewire/rubric-show.blade.php
@@ -9,13 +9,17 @@
 
     <div class="card mb-3">
         <div class="card-header d-flex justify-content-between">
-            <div><i class="bi bi-ui-checks-grid me-2"></i>{{ $rubric->titulo }}</div>
+            <div><i class="bi bi-ui-checks-grid me-2"></i>{{ __('Rubric') }}</div>
             <div>
                 @include('partials.modificar_recursos', ['ruta' => 'rubrics'])
                 @include('partials.editar_recurso', ['recurso' => $rubric, 'ruta' => 'rubrics'])
             </div>
         </div>
-
+        <div class="card-body">
+            <h5 class="card-title">{{ $rubric->titulo }}</h5>
+            <p class="card-text">{{ $rubric->descripcion }}</p>
+        </div>
+        <hr class="my-0">
         @foreach($rubric->criteria_groups as $criteria_group)
             <div class="card-body row pb-0">
                 <div class="col-2">
@@ -42,20 +46,30 @@
                         </button>
                     </div>
                 @endif
-                <div class="col-1 d-flex justify-content-center align-items-center mb-3">
-                    <span class="badge text-bg-secondary fs-6">{{ $criteria_group->total }}</span>
+                <div class="col-auto mb-3">
+                    <button
+                        disabled
+                        class="btn opacity-100 btn-secondary p-3" style="min-width: 4em;">
+                        {{ $criteria_group->total }}
+                    </button>
                 </div>
             </div>
             @if(!$loop->last)
                 <hr class="my-0">
             @endif
         @endforeach
-        <div class="card-footer">
+        <hr class="my-0">
+        <div class="card-body">
             <div class="row">
-                <div class="col">
+                <div class="col-2">
+                    <h5 class="card-title">{{ __('Total') }}</h5>
                 </div>
-                <div class="col-1 d-flex justify-content-center align-items-center py-3">
-                    <span class="badge text-bg-warning fs-6">{{ $rubric->total }}</span>
+                <div class="col text-end">
+                    <button
+                        disabled
+                        class="btn opacity-100 btn-warning p-3" style="min-width: 4em;">
+                        {{ $rubric->total }}
+                    </button>
                 </div>
             </div>
         </div>
