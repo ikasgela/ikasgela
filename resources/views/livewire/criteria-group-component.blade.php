f7179e2e77bbbe748461933fb53262db9f510c5f
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-06-18 19:13:36 +0200

    No mostrar el resultado si no hay criterios

--- resources/views/livewire/criteria-group-component.blade.php
+++ resources/views/livewire/criteria-group-component.blade.php
@@ -69,19 +69,21 @@
             </div>
         </div>
     @endif
-    <div class="col-auto mb-3">
-        @if($rubric_is_editing)
-            <button
-                disabled
-                class="btn opacity-100 btn-secondary p-3" style="min-width: 6em;">
-                {{ $this->max_total }}
-            </button>
-        @else
-            <button
-                disabled
-                class="btn opacity-100 btn-secondary p-3" style="min-width: 6em;">
-                {{ $this->total }}
-            </button>
-        @endif
-    </div>
+    @if($criteria_group->criterias->count() > 0)
+        <div class="col-auto mb-3">
+            @if($rubric_is_editing)
+                <button
+                    disabled
+                    class="btn opacity-100 btn-secondary p-3" style="min-width: 6em;">
+                    {{ $this->max_total }}
+                </button>
+            @else
+                <button
+                    disabled
+                    class="btn opacity-100 btn-secondary p-3" style="min-width: 6em;">
+                    {{ $this->total }}
+                </button>
+            @endif
+        </div>
+    @endif
 </div>
