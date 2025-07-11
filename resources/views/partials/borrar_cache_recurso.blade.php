dce84847e1cb04f27de86e56f3945e68362e711a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-07-11 19:08:35 +0200

    Permitir borrar la caché desde la vista del recurso

--- resources/views/partials/borrar_cache_recurso.blade.php
+++ resources/views/partials/borrar_cache_recurso.blade.php
@@ -1,4 +1,6 @@
-@if(Auth::user()->hasAnyRole(['admin','profesor']) && Route::currentRouteName() == 'actividades.preview')
+@use(Illuminate\Support\Str)
+@if(Auth::user()->hasAnyRole(['admin','profesor'])
+    && (Route::currentRouteName() == 'actividades.preview' || Str::endsWith(Route::currentRouteName(), '.show')))
     <a title="{{ __('Clear cache') }}"
        href="{{ route($ruta .'.borrar_cache', [$recurso->id]) }}"
        class='text-link-light me-2'><i class="fas fa-broom"></i></a>
