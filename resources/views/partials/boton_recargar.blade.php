7774acbe0598b39e33b90ebf9b2ed8f2d8230f6b
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-08-30 17:03:55 +0200

    Icono del botón de recarga

--- resources/views/partials/boton_recargar.blade.php
+++ resources/views/partials/boton_recargar.blade.php
@@ -1,11 +1,11 @@
 @if(Auth::user()->hasRole('alumno'))
     <a href="{{ route('users.home') }}"
        class="btn btn-primary ms-3">
-        <i class="fas fa-redo me-1"></i> {{ __('Reload') }}
+        <i class="bi bi-arrow-clockwise me-1"></i> {{ __('Reload') }}
     </a>
 @elseif(Auth::user()->hasRole('profesor'))
     <a href="{{ route('profesor.index') }}"
        class="btn btn-primary ms-3">
-        <i class="fas fa-redo me-1"></i> {{ __('Reload') }}
+        <i class="bi bi-arrow-clockwise me-1"></i> {{ __('Reload') }}
     </a>
 @endif
