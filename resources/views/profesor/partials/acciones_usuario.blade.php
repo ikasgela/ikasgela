2ced279c8228a877f992d72ce164a6466cf58b3a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-08-31 19:12:34 +0200

    Iconos de la barra de usuario

--- resources/views/profesor/partials/acciones_usuario.blade.php
+++ resources/views/profesor/partials/acciones_usuario.blade.php
@@ -8,20 +8,20 @@
     <div class='btn-group ms-3'>
         <a title="{{ __('Control panel') }}"
            href="{{ route('profesor.tareas', ['user' => $user->id]) }}"
-           class='btn btn-light btn-sm'><i class="fas fa-tasks"></i></a>
+           class='btn btn-light btn-sm'><i class="bi bi-person-gear"></i></a>
 
         {{ html()->form('POST', route('results.alumno'))->open() }}
-        {{ html()->submit('<i class="fas fa-graduation-cap"></i>')->class(['btn btn-light btn-sm', 'rounded-0'])->attribute('title', __('Results')) }}
+        {{ html()->submit('<i class="bi bi-mortarboard"></i>')->class(['btn btn-light btn-sm', 'rounded-0'])->attribute('title', __('Results')) }}
         {{ html()->hidden('user_id', $user->id) }}
         {{ html()->form()->close() }}
 
         {{ html()->form('POST', route('archivo.index'))->open() }}
-        {{ html()->submit('<i class="fas fa-archive"></i>')->class(['btn btn-light btn-sm', 'rounded-0'])->attribute('title', __('Archived')) }}
+        {{ html()->submit('<i class="bi bi-archive"></i>')->class(['btn btn-light btn-sm', 'rounded-0'])->attribute('title', __('Archived')) }}
         {{ html()->hidden('user_id', $user->id) }}
         {{ html()->form()->close() }}
 
         {{ html()->form('POST', route('messages.create-with-subject'))->open() }}
-        {{ html()->submit('<i class="fas fa-envelope"></i>')->class(['btn btn-light btn-sm', 'rounded-start-0'])->attribute('title', __('Message')) }}
+        {{ html()->submit('<i class="bi bi-chat-text"></i>')->class(['btn btn-light btn-sm', 'rounded-start-0'])->attribute('title', __('Message')) }}
         {{ html()->hidden('user_id', $user->id) }}
         {{ html()->form()->close() }}
     </div>
