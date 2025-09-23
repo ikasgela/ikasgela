f8312873c107478c89f8b9bc10eafa8f6b5d42db
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-09-23 10:42:23 +0200

    Copiar el fichero sin procesarlo

--- public/js/single_click.js
+++ public/js/single_click.js
@@ -1,22 +1,18 @@
-/******/ (() => { // webpackBootstrap
-/*!**************************************!*\
-  !*** ./resources/js/single_click.js ***!
-  \**************************************/
 function single_click(event, boton, titulo, subtitulo) {
-  var enviar = true;
-  if (typeof titulo !== 'undefined') {
-    enviar = confirm(titulo + '\n\n' + subtitulo);
-  }
-  var spinner = boton.childNodes[1];
-  if (enviar) {
-    spinner.style.display = "inline-block";
-    boton.classList.add('disabled');
-  } else {
-    event.preventDefault();
-    spinner.style.display = "none";
-    boton.classList.remove('disabled');
-  }
+
+    let enviar = true;
+    if (typeof titulo !== 'undefined') {
+        enviar = confirm(titulo + '\n\n' + subtitulo);
+    }
+
+    const spinner = boton.childNodes[1];
+
+    if (enviar) {
+        spinner.style.display = "inline-block";
+        boton.classList.add('disabled');
+    } else {
+        event.preventDefault();
+        spinner.style.display = "none";
+        boton.classList.remove('disabled');
+    }
 }
-/******/ })()
-;
-//# sourceMappingURL=single_click.js.map
