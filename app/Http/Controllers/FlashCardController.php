264f96ce172b6b5f18b93f9d9c76fde3056f64bd
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-09-30 13:11:47 +0200

    Esqueleto básico de los modelos

---
+++ app/Http/Controllers/FlashCardController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFlashCardRequest;
+use App\Http\Requests\UpdateFlashCardRequest;
+use App\Models\FlashCard;
+
+class FlashCardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFlashCardRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FlashCard $flashCard)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FlashCard $flashCard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFlashCardRequest $request, FlashCard $flashCard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FlashCard $flashCard)
+    {
+        //
+    }
+}
