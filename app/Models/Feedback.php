59f5d9c0942dbc2c348a2f6b9751614b5ec838d5
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-10-04 23:43:50 +0200

    Caché de los feedback

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use YMigVal\LaravelModelCache\HasCachedQueries;
 
 /**
  * @mixin IdeHelperFeedback
@@ -11,6 +12,7 @@ use Illuminate\Database\Eloquent\Model;
 class Feedback extends Model
 {
     use HasFactory;
+    use HasCachedQueries;
 
     protected $fillable = [
         'comentable_id', 'comentable_type',
