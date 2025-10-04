17d82c7bde7fd2762fb3ee2750da2e13950a218a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-10-04 21:32:55 +0200

    Mixins que faltan

--- app/Models/FlashCard.php
+++ app/Models/FlashCard.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @mixin IdeHelperFlashCard
+ */
 class FlashCard extends Model
 {
     use HasFactory;
