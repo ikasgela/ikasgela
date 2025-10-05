f2fdfde07bb785fe2d8e576e263700829b5475d8
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-10-05 15:09:48 +0200

    Mostrar siempre el total de la rúbrica

--- resources/views/livewire/rubric-show.blade.php
+++ resources/views/livewire/rubric-show.blade.php
@@ -100,31 +100,29 @@
                 </button>
             </div>
         @endif
-        @if($rubric->completada || $rubric_is_editing)
-            <hr class="my-0">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-2">
-                        <h5 class="card-title">{{ __('Total') }}</h5>
-                    </div>
-                    <div class="col text-end">
-                        @if($rubric->completada)
-                            <button
-                                disabled
-                                class="btn opacity-100 btn-warning p-3 me-2" style="min-width: 6em;">
-                                {{ $this->total > 0 ? formato_decimales($this->total / $this->max_total * 100) : 0 }}
-                                /100
-                            </button>
-                        @endif
+        <hr class="my-0">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-2">
+                    <h5 class="card-title">{{ __('Total') }}</h5>
+                </div>
+                <div class="col text-end">
+                    @if($rubric->completada)
                         <button
                             disabled
-                            class="btn opacity-100 btn-secondary p-3" style="min-width: 6em;">
-                            {{ $rubric->completada ? $this->total . '/' : '' }}{{ $this->max_total }}
+                            class="btn opacity-100 btn-warning p-3 me-2" style="min-width: 6em;">
+                            {{ $this->total > 0 ? formato_decimales($this->total / $this->max_total * 100) : 0 }}
+                            /100
                         </button>
-                    </div>
+                    @endif
+                    <button
+                        disabled
+                        class="btn opacity-100 btn-secondary p-3" style="min-width: 6em;">
+                        {{ $rubric->completada ? $this->total . '/' : '' }}{{ $this->max_total }}
+                    </button>
                 </div>
             </div>
-        @endif
+        </div>
     </div>
     @include('layouts.errors')
 </div>
