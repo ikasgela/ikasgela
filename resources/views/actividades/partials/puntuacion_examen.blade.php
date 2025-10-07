1db59a5943dc81c9b915cd6bafc8ceaeccab3db2
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-10-07 11:22:33 +0200

    No mostrar la puntuación de actividades expiradas

--- resources/views/actividades/partials/puntuacion_examen.blade.php
+++ resources/views/actividades/partials/puntuacion_examen.blade.php
@@ -1,3 +1,7 @@
-<div class="d-inline-flex align-items-center">
-    <span class="me-2">{{ __('Relative value') }}: {{ $actividad->puntos() }}/{{ $user->puntuacion_actividades_en_curso_examen() }}</span>
-</div>
+@if(!$actividad->is_expired)
+    <div class="d-inline-flex align-items-center">
+        <span class="me-2">
+            {{ __('Relative value') }}: {{ $actividad->puntos() }}/{{ $user->puntuacion_actividades_en_curso_examen() }}
+        </span>
+    </div>
+@endif
