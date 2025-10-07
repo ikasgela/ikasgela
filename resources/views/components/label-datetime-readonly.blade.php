737d3fc8e7023ea73561c9e6242982c87a2620fb
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-10-07 09:07:50 +0200

    Nuevo componente para editar fechas

---
+++ resources/views/components/label-datetime-readonly.blade.php
@@ -0,0 +1,8 @@
+<div class="row mb-3">
+    <div class="col-sm-2">
+        {{ html()->label($label, $name)->class('col-form-label') }}
+    </div>
+    <div class="col-sm-10">
+        {{ html()->datetime($name)->isReadonly()->class('form-control-plaintext') }}
+    </div>
+</div>
