d3dd9490d3ec3b4a1eacc4eaf3d3a69bfd2cf95c
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-10-07 09:49:46 +0200

    Fecha de solo lectura mejorada

--- resources/views/components/label-datetime-readonly.blade.php
+++ resources/views/components/label-datetime-readonly.blade.php
@@ -3,6 +3,10 @@
         {{ html()->label($label, $name)->class('col-form-label') }}
     </div>
     <div class="col-sm-10">
-        {{ html()->datetime($name)->isReadonly()->class('form-control-plaintext') }}
+        @if(!is_null(html()->value($name)))
+            {{ html()->datetime($name)->isReadonly()->class('form-control-plaintext') }}
+        @else
+            <span class="form-control-plaintext">{{ __('Undefined') }}</span>
+        @endif
     </div>
 </div>
