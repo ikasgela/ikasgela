0a1e5f24684beb7c8042c66488e63eef4413cdb4
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-10-30 10:03:00 +0100

    Validar el dominio del correo al reiniciar la contraseña

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
@@ -28,5 +29,25 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    protected function validateEmail(Request $request)
+    {
+        switch (subdominio()) {
+            case 'egibide':
+                $dominios = 'egibide.org,ikasle.egibide.org';
+                $validator = 'allowed_domains';
+                break;
+            case 'deusto':
+                $dominios = 'deusto.es,opendeusto.es';
+                $validator = 'allowed_domains';
+                break;
+            default:
+                $dominios = 'egibide.org,ikasle.egibide.org,deusto.es,opendeusto.es';
+                $validator = 'forbidden_domains';
+                break;
+        }
+
+        $request->validate(['email' => "required|string|email:rfc,dns|$validator:$dominios|max:255"]);
     }
 }
