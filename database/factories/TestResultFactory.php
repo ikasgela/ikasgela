b7c3d102c58a7cb6baaaf0f223180859ebd3a62d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-11-10 10:07:42 +0100

    Factory y seeder

---
+++ database/factories/TestResultFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Curso;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TestResult>
+ */
+class TestResultFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'titulo' => fake()->words(3, true),
+            'descripcion' => fake()->sentence(8),
+            'curso_id' => Curso::factory(),
+        ];
+    }
+}
