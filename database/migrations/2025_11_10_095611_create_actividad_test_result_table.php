2798db4f2d7e23f0ee68b1a1b53127c1036bf4c2
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-11-10 10:00:26 +0100

    Migraciones

---
+++ database/migrations/2025_11_10_095611_create_actividad_test_result_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('actividad_test_result', function (Blueprint $table) {
+            $table->id();
+
+            $table->uuid('orden')->index()->nullable();
+            $table->boolean('titulo_visible')->default(true)->nullable();
+            $table->boolean('descripcion_visible')->default(true)->nullable();
+            $table->integer('columnas')->default(6)->nullable();
+
+            $table->foreignId('test_result_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('actividad_id')->constrained('actividades')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('actividad_test_result');
+    }
+};
