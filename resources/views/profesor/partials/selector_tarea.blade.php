7cad633dcd73e9f77c82b0c57219be52dc077f58
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2026-01-27 14:29:40 +0100

    Títulos de siguiente/anterior

--- resources/views/profesor/partials/selector_tarea.blade.php
+++ resources/views/profesor/partials/selector_tarea.blade.php
@@ -1,6 +1,7 @@
 <div class="me-3">
     @if(isset($actividad_anterior))
         <a class="btn btn-primary"
+           title="{{ __('Previous') }}"
            href="{{ route('profesor.revisar', ['user' => $user->id, 'tarea' => $actividad_anterior]) }}">
             <i class="bi bi-arrow-left"></i>
         </a>
@@ -11,6 +12,7 @@
     @endif
     @if(isset($actividad_siguiente))
         <a class="btn btn-primary"
+           title="{{ __('Next') }}"
            href="{{ route('profesor.revisar', ['user' => $user->id, 'tarea' => $actividad_siguiente]) }}">
             <i class="bi bi-arrow-right"></i>
         </a>
