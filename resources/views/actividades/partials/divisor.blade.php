092b8ddc6c578b9a0fd6dd90da0cd9c5ffe1af67
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2026-02-08 23:59:10 +0100

    Divisor de altura fija

--- resources/views/actividades/partials/divisor.blade.php
+++ resources/views/actividades/partials/divisor.blade.php
@@ -1,5 +1,5 @@
 @if($actividad->divisor)
     <tr class="text-bg-warning">
-        <td colspan="100" class="p-1"></td>
+        <td colspan="100" class="p-0" @style(['min-height:.5rem', 'height:.5rem'])></td>
     </tr>
 @endif
