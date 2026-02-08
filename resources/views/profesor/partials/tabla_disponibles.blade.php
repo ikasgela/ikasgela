a58d4f1f5d6107ff020d0b1b669954795b4a0cc7
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2026-02-08 10:41:38 +0100

    Mostrar el estado de revisión en la lista de disponibles

--- resources/views/profesor/partials/tabla_disponibles.blade.php
+++ resources/views/profesor/partials/tabla_disponibles.blade.php
@@ -9,7 +9,8 @@
             <th>#</th>
             <th>{{ __('Name') }}</th>
             <th>{{ __('Slug') }}</th>
-            <th>{{ __('Score') }}</th>
+            <th class="p-0"></th>
+            <th class="text-center">{{ __('Score') }}</th>
             <th class="text-center">{{ __('Auto') }}</th>
             <th>{{ __('Next') }}</th>
             @if(Auth::user()->hasRole('admin'))
@@ -45,7 +46,10 @@
                     @include('actividades.partials.caducada')
                 </td>
                 <td class="clickable">{{ $actividad->unidad->slug.'/'.$actividad->slug }}</td>
-                <td class="clickable">{{ formato_decimales($actividad->puntuacion * ($actividad->multiplicador ?: 1)) }}</td>
+                <td class="p-0 ps-1 {{ !$actividad->fecha_revision ? 'bg-warning' : '' }}"
+                    title="{{ !$actividad->fecha_revision ? __('Not reviewed') : '' }}">&nbsp;
+                </td>
+                <td class="text-center clickable">{{ formato_decimales($actividad->puntuacion * ($actividad->multiplicador ?: 1)) }}</td>
                 <td class="text-center clickable">{!! $actividad->auto_avance ? '<i class="bi bi-check-lg text-success"></i>' : '<i class="bi bi-x text-danger"></i>' !!}</td>
                 @include('profesor.partials.siguiente_actividad')
                 @if(Auth::user()->hasRole('admin'))
